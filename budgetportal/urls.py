01f24fb7c6bc7aa3c87b045fcd09f6ffea9dfbed
JD Bothma <jbothma@gmail.com>
2017-11-21 22:25:59 +0200

    Start fetching department

--- budgetportal/urls.py
+++ budgetportal/urls.py
@@ -1,8 +1,22 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
+from django.views.decorators.cache import cache_page
 
-urlpatterns = patterns('',
+from . import views
+
+CACHE_SECS = 12 * 60 * 60
+
+urlpatterns = patterns(
+    '',
     url(r'^$', 'budgetportal.views.home', name='home'),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?P<financial_year>[\w-]+)'
+        '/national'
+        '/departments'
+        '/(?P<department_name_slug>[\w-]+)$', cache_page(CACHE_SECS)(views.department),
+        kwargs={'sphere': 'national', 'geographic_region_slug': 'south-africa'}),
+    url(r'^(?P<financial_year>[\w]+)'
+        '/(?P<sphere>[\w]+)'
+        '/(?P<geographic_region_slug>[\w_]+)'
+        '/departments'
+        '/(?P<department_slug>[\w_]+)$', cache_page(CACHE_SECS)(views.department)),
 )
--- budgetportal/views.py
+++ budgetportal/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from ckanapi import RemoteCKAN
+import yaml
+ckan = RemoteCKAN('https://treasurydata.openup.org.za')
 
-def home(request):
-    return render(request, 'index.html')
+
+def department(request, financial_year, sphere, geographic_region_slug, department_name_slug):
+        raise Exception("Department not found")
+    department_package = response['results'][0]
+    financial_years_context = []
+    # for year in financial_years:
+    #     closest_match, closest_is_exact = year.get_closest_match(department)
+    #     financial_years_context.append({
+    #         'id': year.id,
+    #         'is_selected': year == department.government.sphere.financial_year,
+    #         'closest_match': {
+    #             'name': closest_match.name,
+    #             'slug': closest_match.slug,
+    #             'url_path': closest_match.get_url_path(),
+    #             'organisational_unit': closest_match.organisational_unit,
+    #             'is_exact_match': closest_is_exact,
+    #         },
+    #     })
+    # financial_years_context = sorted(financial_years_context, key=lambda y: y['id'])
+    context = {
+        'name': department_package['name'],
+    #     'slug': department.slug,
+    #     'vote_number': department.vote_number,
+    #     'government': {
+    #         'name': department.government.name,
+    #         'slug': department.government.slug,
+    #     },
+    #     'financial_years': financial_years_context,
+    #     'narrative': department.narrative,
+    }
+
+    response_yaml = yaml.safe_dump(context, default_flow_style=False, encoding='utf-8')
+    return HttpResponse(response_yaml)
