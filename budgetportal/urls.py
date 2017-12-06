6a56e1d6ed4060463832656fbb00d0d563bc7c58
JD Bothma <jbothma@gmail.com>
2017-12-06 17:05:26 +0200

    django-1.10.8

--- budgetportal/urls.py
+++ budgetportal/urls.py
@@ -1,15 +1,13 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.decorators.cache import cache_page
 
 from . import views
 
 CACHE_SECS = 12 * 60 * 60
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', 'budgetportal.views.home', name='home'),
 
+urlpatterns = [
     # Department List
-    url(r'^(?P<financial_year>[\w-]+)'
+    url(r'^(?P<financial_year_id>[\w-]+)'
         '/departments.yaml', cache_page(CACHE_SECS)(views.department_list)),
-)
+]
