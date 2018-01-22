c9afe88a02b7f7146acdd60dc7ff2034a28422e6
JD Bothma <jbothma@gmail.com>
2018-01-22 17:26:38 +0200

    Add support for national functions

--- budgetportal/management/commands/load_functions.py
+++ budgetportal/management/commands/load_functions.py
@@ -9,26 +9,33 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('filename', type=str)
+        parser.add_argument('financial_year', type=str)
 
     def handle(self, *args, **options):
         with open(options['filename']) as csvfile:
             with open('missing_programmes.csv', 'w') as missing_programmes:
-                reader = csv.DictReader(csvfile)
-                functions = GovtFunction.objects.all()
-                functions_by_slug = {}
-                for function in functions:
-                    functions_by_slug[function.slug] = function
+                with open('missing_functions.csv', 'w') as missing_functions:
+                    reader = csv.DictReader(csvfile)
+                    functions = GovtFunction.objects.all()
+                    functions_by_slug = {}
+                    for function in functions:
+                        functions_by_slug[function.slug] = function
 
-                for row in reader:
-                    programmes = Programme.objects.filter(
-                        slug=slugify(row['Programme']),
-                        department__slug=slugify(row['Department']),
-                        department__government__slug=slugify(row['Province']),
-                    )
-                    function = functions_by_slug[slugify(row['Function group'])]
-                    if programmes.count():
-                        for programme in programmes:
-                            programme.govt_functions.add(function)
-                            programme.save()
-                    else:
-                        missing_programmes.write("%r\n" % row)
+                    for row in reader:
+                        programmes = Programme.objects.filter(
+                            slug=slugify(row['Programme']),
+                            department__slug=slugify(row['Department']),
+                            department__government__slug=slugify(row['Government']),
+                            department__government__sphere__financial_year__slug=options['financial_year'],
+                        )
+                        function_slug = slugify(row['Function group'])
+                        if function_slug in functions_by_slug:
+                            function = functions_by_slug[function_slug]
+                            if programmes.count():
+                                for programme in programmes:
+                                    programme.govt_functions.add(function)
+                                    programme.save()
+                            else:
+                                missing_programmes.write("%r\n" % row)
+                        else:
+                            missing_functions.write("%s\n" % function_slug)
