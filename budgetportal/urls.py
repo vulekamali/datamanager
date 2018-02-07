e7b0e0ee7e96d8fdbf6db670cf88946d2ebd3a7c
JD Bothma <jbothma@gmail.com>
2018-02-07 08:33:44 +0200

    Generate routing and data for non-data-driven pages

--- budgetportal/urls.py
+++ budgetportal/urls.py
@@ -13,6 +13,43 @@ CACHE_SECS = 0
 
 urlpatterns = [
 
+    url(r'^(?P<financial_year_id>[\w-]+).yaml',
+        cache_page(CACHE_SECS)(views.Page.as_view(
+            slug='index',
+            organisational_unit='financial_year',
+        ))),
+    url(r'^index.yaml',
+        cache_page(CACHE_SECS)(views.Page.as_view(
+            slug='index',
+            organisational_unit='financial_year',
+        ))),
+
+    url(r'^(?P<financial_year_id>[\w-]+)/about.yaml',
+        cache_page(CACHE_SECS)(views.Page.as_view(
+            slug='about',
+            organisational_unit='about',
+        ))),
+    url(r'^(?P<financial_year_id>[\w-]+)/glossary.yaml',
+        cache_page(CACHE_SECS)(views.Page.as_view(
+            slug='glossary',
+            organisational_unit='learning',
+        ))),
+    url(r'^(?P<financial_year_id>[\w-]+)/resources.yaml',
+        cache_page(CACHE_SECS)(views.Page.as_view(
+            slug='resources',
+            organisational_unit='learning',
+        ))),
+    url(r'^(?P<financial_year_id>[\w-]+)/search-result.yaml',
+        cache_page(CACHE_SECS)(views.Page.as_view(
+            slug='search-result',
+            organisational_unit='financial_year',
+        ))),
+    url(r'^(?P<financial_year_id>[\w-]+)/videos.yaml',
+        cache_page(CACHE_SECS)(views.Page.as_view(
+            slug='videos',
+            organisational_unit='learning',
+        ))),
+
     # Department List
     url(r'^(?P<financial_year_id>[\w-]+)'
         '/departments.yaml', cache_page(CACHE_SECS)(views.department_list)),
@@ -28,7 +65,7 @@ urlpatterns = [
         '/(?P<government_slug>[\w-]+)'
         '/departments'
         '/(?P<department_slug>[\w-]+).yaml$', cache_page(CACHE_SECS)(views.department)),
-    
+
     # Contributed Dataset List
     url(r'^(?P<financial_year_id>[\w-]+)'
         '/contributed-data.yaml', cache_page(CACHE_SECS)(views.contributed_dataset_list)),
