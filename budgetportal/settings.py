efe84bde50fbe166f8bc820403723d86d1e9697a
JD Bothma <jbothma@gmail.com>
2018-03-22 12:51:30 +0200

    Add providers

--- budgetportal/settings.py
+++ budgetportal/settings.py
@@ -49,8 +49,11 @@ INSTALLED_APPS = (
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.google',
-
+    'allauth.socialaccount.providers.linkedin_oauth2',
+    'allauth.socialaccount.providers.twitter',
 )
 
 MIDDLEWARE_CLASSES = (
