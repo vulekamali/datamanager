982e636012fd41007a03fb8451bd78550733c1e7
JD Bothma <jbothma@gmail.com>
2018-10-23 12:35:44 +0200

    Instantiate list in constructor so the python interpreter doesn't share it

--- budgetportal/bulk_upload.py
+++ budgetportal/bulk_upload.py
@@ -3,7 +3,6 @@ Admin View for bulk uploading
 """
 
 from django import forms
-# from forms import formset_factory
 from django.shortcuts import render
 from django.utils.text import slugify
 from io import BytesIO
@@ -13,6 +12,9 @@ from budgetportal.models import (
     Government,
     Department,
 )
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class FileForm(forms.Form):
@@ -50,55 +52,59 @@ def bulk_upload_view(request):
 
 
 class Preview:
-    rows = []
 
     def __init__(self, sphere, metadata_file):
-        wb = load_workbook(filename=BytesIO(metadata_file.read()))
-        ws = wb['Resources']
-        rows = list(ws.rows)
-        headings = rows[0]
-        rows = rows[1:]
-        heading_index = {}
-        for i, cell in enumerate(headings):
-            if cell.value:
-                heading_index[cell.value] = i
+        self.rows = []
 
-        government_idx = heading_index['government']
-        department_name_idx = heading_index[u'department_name']
-        dataset_name_idx = heading_index['dataset_name']
-        dataset_title_idx = heading_index['dataset_title']
-        group_id_idx = heading_index['group_id']
-        resource_name_idx = heading_index['resource_name']
-        resource_format_idx = heading_index['resource_format']
-        resource_url_idx = heading_index['resource_url']
+        with BytesIO(metadata_file.read()) as bytes_io:
+            wb = load_workbook(filename=bytes_io, read_only=True)
+            ws = wb['Resources']
 
-        for row in rows:
-            if not row[government_idx].value:
-                continue
-            government = Government.objects.get(sphere=sphere,
-                                                name=row[government_idx].value)
-            department = Department.objects.get(government=government,
-                                                name=row[department_name_idx].value)
-            self.rows.append({
-                'government': {
-                    'object': government,
-                    'label': government.name,
-                },
-                'department': {
-                    'object': department,
-                    'label': department.name,
-                },
-                'dataset': {
-                    'name': slugify(row[dataset_name_idx].value),
-                    'label': row[dataset_title_idx].value,
-                },
-                'group': {
-                    'name': slugify(row[group_id_idx].value),
-                },
-                'resource': {
-                    'name': row[resource_name_idx].value,
-                    'format': row[resource_format_idx].value,
-                    'url': row[resource_url_idx].value,
-                    'valid': True,
-                },
-            })
+            for row_idx, ws_row in enumerate(ws.rows):
+
+                if not ws_row[0].value:
+                    continue
+
+                if row_idx == 0:
+                    heading_index = {}
+                    for i, cell in enumerate(ws_row):
+                        if cell.value:
+                            heading_index[cell.value] = i
+
+                    government_idx = heading_index['government']
+                    department_name_idx = heading_index[u'department_name']
+                    dataset_name_idx = heading_index['dataset_name']
+                    dataset_title_idx = heading_index['dataset_title']
+                    group_id_idx = heading_index['group_id']
+                    resource_name_idx = heading_index['resource_name']
+                    resource_format_idx = heading_index['resource_format']
+                    resource_url_idx = heading_index['resource_url']
+
+                else:
+                    government = Government.objects.get(sphere=sphere,
+                                                        name=ws_row[government_idx].value)
+                    department = Department.objects.get(government=government,
+                                                        name=ws_row[department_name_idx].value)
+                    self.rows.append({
+                        'government': {
+                            'object': government,
+                            'label': government.name,
+                        },
+                        'department': {
+                            'object': department,
+                            'label': department.name,
+                        },
+                        'dataset': {
+                            'name': slugify(ws_row[dataset_name_idx].value),
+                            'label': ws_row[dataset_title_idx].value,
+                        },
+                        'group': {
+                            'name': slugify(ws_row[group_id_idx].value),
+                        },
+                        'resource': {
+                            'name': ws_row[resource_name_idx].value,
+                            'format': ws_row[resource_format_idx].value,
+                            'url': ws_row[resource_url_idx].value,
+                            'valid': True,
+                        },
+                    })
