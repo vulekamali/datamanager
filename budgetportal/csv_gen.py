24c7ec516ff4385017cc08a84523311f095fab21
JD Bothma <jbothma@gmail.com>
2018-11-06 11:28:39 +0200

    Document why we're not using writeheader

--- budgetportal/csv_gen.py
+++ budgetportal/csv_gen.py
@@ -16,6 +16,8 @@ def generate_csv_response(data):
     """
     :param data: dict
     :return: StreamingHttpResponse object
+
+    Roughly based on https://docs.djangoproject.com/en/2.1/howto/outputting-csv/#streaming-large-csv-files
     """
     response = StreamingHttpResponse(streaming_content=iter_items(data['cells'], Echo()),
                                      content_type='text/csv')
@@ -27,9 +29,12 @@ def generate_csv_response(data):
 def iter_items(cells, pseudo_buffer):
     headers_list = cells[0].keys()
     headers_dict = {}
+    writer = csv.DictWriter(pseudo_buffer, fieldnames=sorted(headers_list))
+
+    # Not using writer.writeheader because it's not working - not sure why
     for header in headers_list:
         headers_dict[header] = header
-    writer = csv.DictWriter(pseudo_buffer, fieldnames=sorted(headers_list))
     yield writer.writerow(headers_dict)
+
     for dict_object in cells:
         yield writer.writerow(dict_object)
