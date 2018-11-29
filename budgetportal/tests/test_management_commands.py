25a601f9c655a50b521c502379ab1a60ca4d0c7d
JD Bothma <jbothma@gmail.com>
2018-11-29 18:15:56 +0200

    Add test for load_departments manage command

---
+++ budgetportal/tests/test_management_commands.py
@@ -0,0 +1,44 @@
+from budgetportal.models import (
+    FinancialYear,
+    Sphere,
+    Government,
+    Department,
+)
+from django.core.management import call_command
+from django.test import TestCase
+
+
+class BasicPagesTestCase(TestCase):
+    def setUp(self):
+        year = FinancialYear.objects.create(slug="2030-31")
+
+        # spheres
+        national = Sphere.objects.create(financial_year=year, name='National')
+        provincial = Sphere.objects.create(financial_year=year, name='Provincial')
+
+        # governments
+        self.south_africa = Government.objects.create(sphere=national, name='South Africa')
+        self.fake_central_province = Government.objects.create(
+            sphere=provincial,
+            name='Fake Central'
+        )
+        self.fake_northeast_province = Government.objects.create(
+            sphere=provincial,
+            name='Fake North-East'
+        )
+
+    def test_load_departments_national(self):
+        filename = 'budgetportal/tests/test_management_commands_national_departments.csv'
+        call_command('load_departments', '2030-31', 'national', filename)
+
+        presidency = Department.objects.get(government=self.south_africa, name='The Presidency')
+        self.assertEqual(presidency.vote_number, 1)
+        self.assertTrue(presidency.is_vote_primary)
+        self.assertIn("To serve the president", presidency.intro)
+        self.assertIn("Facilitate a common", presidency.intro)
+
+        cpsi = Department.objects.get(government=self.south_africa, vote_number=10)
+        self.assertEqual(cpsi.name, 'Centre for Public Service Innovation')
+        self.assertFalse(cpsi.is_vote_primary)
+        self.assertIn("Facilitate the unearthing", cpsi.intro)
+        self.assertIn("The responsibility for", cpsi.intro)
