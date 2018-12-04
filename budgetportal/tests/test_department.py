c91d5a9b1c146543adc5a4ff9b9a4a9620d11e77
JD Bothma <jbothma@gmail.com>
2018-12-04 10:37:12 +0200

    WIP fully mocked adjustments summary test

--- budgetportal/tests/test_department.py
+++ budgetportal/tests/test_department.py
@@ -8,6 +8,7 @@ from budgetportal.models import (
     Government,
     Department,
 )
+from budgetportal import models
 from django.test import TestCase
 from mock import Mock
 
@@ -31,7 +32,7 @@ class AdjustedBudgetMissingTestCase(TestCase):
         self.assertEqual(self.department.get_adjusted_budget_summary(), None)
 
 
-class AdjustedBudgeOpenSpendingtMissingTestCase(TestCase):
+class AdjustedBudgetOpenSpendingtMissingTestCase(TestCase):
     """Unit tests of adjusted budget data summary for a department"""
     def setUp(self):
         year = FinancialYear(slug="2030-31")
@@ -49,4 +50,47 @@ class AdjustedBudgeOpenSpendingtMissingTestCase(TestCase):
         self.department.get_adjusted_estimates_expenditure_dataset = Mock(return_value=mock_dataset)
 
     def test_missing_openspending_dataset(self):
+        self.assertEqual(self.department.get_adjusted_budget_summary(), None)
+
+
+class AdjustedBudgetTestCase(TestCase):
+    """Unit tests of adjusted budget data summary for a department"""
+    def setUp(self):
+        year = FinancialYear(slug="2030-31")
+        sphere = Sphere(financial_year=year, name="A sphere")
+        government = Government(sphere=sphere, name="A government")
+        self.department = Department(
+            government=government,
+            name="Fake",
+            vote_number=1,
+            is_vote_primary=True,
+            intro="",
+        )
+        mock_dataset = Mock()
+        mock_openspending_api = Mock()
+        self.mock_openspending_api = mock_openspending_api
+        mock_openspending_api.get_adjustment_kind_ref = Mock
+        mock_openspending_api.get_phase_ref = Mock
+        mock_openspending_api.get_programme_name_ref = Mock
+        mock_openspending_api.get_department_name_ref = Mock(return_value='department_name_ref')
+        mock_openspending_api.get_financial_year_ref = Mock(return_value="financial_year_ref")
+        mock_openspending_api.aggregate = Mock
+        mock_openspending_api.filter_dept = Mock(return_value={'cells': []})
+        mock_openspending_api.filter_by_ref_exclusion = Mock
+        mock_openspending_api.aggregate_by_ref = Mock
+        mock_openspending_api.aggregate_url = Mock
+        mock_dataset.get_openspending_api = Mock(return_value=mock_openspending_api)
+        self.department.get_adjusted_estimates_expenditure_dataset = Mock(return_value=mock_dataset)
+        self.department.get_financial_year = Mock(return_value=year)
+        self.department._get_adjustments_by_econ_class = Mock(return_value=Mock())
+        self.department._get_adjustments_by_programme = Mock(return_value=Mock())
+        self.department._get_adjustments_by_type = Mock(return_value=Mock())
+        self.department._get_budget_virements = Mock(return_value=Mock())
+        self.department._get_budget_special_appropriations = Mock(return_value=Mock())
+        self.department._get_budget_direct_charges = Mock(return_value=Mock())
+        models.csv_url = Mock(return_value=Mock())
+
+
+    def test_no_adjustment(self):
+        self.department._get_total_budget_adjustment = Mock(return_value=(123, 123))
         self.assertEqual(self.department.get_adjusted_budget_summary(), None)
