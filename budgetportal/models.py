e38cb15f63a298badbe431fd4ead32c89015a7ca
Jaron Rademeyer <jaron.rademeyer@poort.co>
2018-12-11 11:44:15 +0200

    * Add EXPENDITURE_TIME_SERIES_PHASES list constant * Iterate over each year and phase to build expenditure dict

--- budgetportal/models.py
+++ budgetportal/models.py
@@ -69,6 +69,13 @@ TRENDS_AND_ESTIMATES_PHASES = [
     'Medium Term Estimates',
 ]
 
+EXPENDITURE_TIME_SERIES_PHASES = (
+    'Main appropriation',
+    'Adjusted appropriation',
+    'Final Appropriation',
+    'Audited Outcome'
+)
+
 
 class FinancialYear(models.Model):
     organisational_unit = 'financial_year'
@@ -1118,31 +1125,31 @@ class Department(models.Model):
 
         if result['cells']:
             cpi = get_cpi()
-            for idx, financial_year_start in enumerate(financial_year_starts):
-                phase = TRENDS_AND_ESTIMATES_PHASES[idx]
-                cell = [
-                    c for c in result['cells']
-                    if c[openspending_api.get_financial_year_ref()] == int(financial_year_start)
-                       and c[openspending_api.get_phase_ref()] == phase
-                ][0]
-                nominal = cell['value.sum']
-                expenditure['nominal'].append({
-                    'financial_year': FinancialYear.slug_from_year_start(financial_year_start),
-                    'amount'        : nominal,
-                    'phase'         : phase,
-                })
-                expenditure['real'].append({
-                    'financial_year': FinancialYear.slug_from_year_start(financial_year_start),
-                    'amount'        : int((Decimal(nominal) / cpi[financial_year_start]['index']) * 100),
-                    'phase'         : phase,
-                })
+            for financial_year_start in financial_year_starts:
+                for phase in EXPENDITURE_TIME_SERIES_PHASES:
+                    cell = [
+                        c for c in result['cells']
+                        if c[openspending_api.get_financial_year_ref()] == int(financial_year_start)
+                           and c[openspending_api.get_phase_ref()] == phase
+                    ][0]
+                    nominal = cell['value.sum']
+                    expenditure['nominal'].append({
+                        'financial_year': FinancialYear.slug_from_year_start(financial_year_start),
+                        'amount'        : nominal,
+                        'phase'         : phase,
+                    })
+                    expenditure['real'].append({
+                        'financial_year': FinancialYear.slug_from_year_start(financial_year_start),
+                        'amount'        : int((Decimal(nominal) / cpi[financial_year_start]['index']) * 100),
+                        'phase'         : phase,
+                    })
 
             return {
                 'expenditure'        : expenditure,
                 'dataset_detail_page': dataset.get_url_path(),
             }
         else:
-            logger.warning("Missing expenditure data for %r budget year %s",
+            logger.warning("Missing expenditure time series data for %r budget year %s",
                            cuts, self.get_financial_year().slug)
             return None
 
