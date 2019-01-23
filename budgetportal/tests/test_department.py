1add2c1310bc99a0c4b460348b8ef65e78284051
Jaron Rademeyer <jaronrademeyer@gmail.com>
2019-01-23 10:15:13 +0200

    * Add additional test data * Add test for budgeted and actual expenditure programme breakdown

--- budgetportal/tests/test_department.py
+++ budgetportal/tests/test_department.py
@@ -11,7 +11,7 @@ from budgetportal.models import (
 from budgetportal import models
 from django.test import TestCase
 from mock import Mock
-
+import json
 
 class AdjustedBudgetMissingTestCase(TestCase):
     """Unit tests of adjusted budget data summary for a department"""
@@ -110,6 +110,8 @@ class BudgetedAndActualExpenditureSummaryTestCase(TestCase):
     """Unit tests of budgeted and actual expenditure summary for a department"""
 
     def setUp(self):
+        with open('budgetportal/tests/test_data/budget_and_actual.json', 'r') as mock_data:
+            self.mock_data = json.load(mock_data)
         year = FinancialYear(slug="2018-19")
         sphere = Sphere(financial_year=year, name="A sphere")
         government = Government(sphere=sphere, name="A government")
@@ -124,102 +126,31 @@ class BudgetedAndActualExpenditureSummaryTestCase(TestCase):
         self.mock_openspending_api = Mock()
         self.mock_openspending_api.get_adjustment_kind_ref = Mock(return_value='adjustment_kind_ref')
         self.mock_openspending_api.get_phase_ref = Mock(return_value='budget_phase.budget_phase')
-        self.mock_openspending_api.get_programme_name_ref = Mock
+        self.mock_openspending_api.get_programme_name_ref = Mock(return_value='programme_number.programme')
         self.mock_openspending_api.get_department_name_ref = Mock(return_value='department_name_ref')
         self.mock_openspending_api.get_financial_year_ref = Mock(return_value="financial_year.financial_year")
         self.mock_openspending_api.aggregate = Mock(return_value={'cells': [{'value.sum': 1, '_count': 0}]})
-        self.mock_openspending_api.filter_dept = Mock(return_value={'cells': []})
-        self.mock_openspending_api.filter_by_ref_exclusion = Mock
-        self.mock_openspending_api.aggregate_by_three_ref = Mock(return_value=test_cells_data_complete)
+        self.mock_openspending_api.filter_dept = Mock(return_value={'cells': self.mock_data['program_test_cells_complete']})
         self.mock_openspending_api.aggregate_url = Mock
         mock_dataset.get_openspending_api = Mock(return_value=self.mock_openspending_api)
         self.department.get_expenditure_time_series_dataset = Mock(return_value=mock_dataset)
         self.department.get_financial_year = Mock(return_value=year)
 
     def test_no_cells_null_response(self):
-        self.mock_openspending_api.aggregate_by_three_ref = Mock(return_value=[])
-        result = self.department.get_expenditure_time_series_summary()
+        self.mock_openspending_api.filter_dept = Mock(return_value={'cells': []})
+        result = self.department.get_expenditure_time_series_by_programme()
         self.assertEqual(result, None)
 
     def test_complete_data_no_notices(self):
-        result = self.department.get_expenditure_time_series_summary()
+        result = self.department.get_expenditure_time_series_by_programme()
         self.assertEqual(result['notices'], [])
 
-    def test_missing_data_not_published(self):
-        """ Here we feed an incomplete set of cells and expect it to tell us that 2018 data has not been published """
-        self.mock_openspending_api.aggregate = Mock(return_value={'cells': [{'value.sum': 0, '_count': 0}]})
-        self.mock_openspending_api.aggregate_by_three_ref = Mock(return_value=test_cells_data_missing_2018)
-        result = self.department.get_expenditure_time_series_summary()
-        self.assertEqual(result['notices'], ['Please note that the data for 2018 has not been published on vulekamali.'])
-
-    def test_missing_data_dept_did_not_exist(self):
+    def test_missing_data_prog_did_not_exist(self):
         """ Here we feed an incomplete set of cells and expect it to tell us that the department did not exist
         (removed 2018 data) """
         self.mock_openspending_api.aggregate = Mock(return_value={'cells': [{'value.sum': 1, '_count': 0}]})
-        self.mock_openspending_api.aggregate_by_three_ref = Mock(return_value=test_cells_data_missing_2018)
-        result = self.department.get_expenditure_time_series_summary()
-        self.assertEqual(result['notices'], ['This department did not exist for some years displayed.'])
+        self.mock_openspending_api.filter_dept = Mock(return_value={'cells': self.mock_data['program_test_cells_missing_2018_revenue_admin']})
+        result = self.department.get_expenditure_time_series_by_programme()
+        self.assertEqual(result['notices'], ['One or more programmes did not exist for some years displayed.'])
 
 
-test_cells_data_missing_2018 = [{
-    'budget_phase.budget_phase': 'Adjusted appropriation',
-    'vote_number.department': 'National Treasury', '_count': 557,
-    'financial_year.financial_year': 2015, 'value.sum': 28726061000
-}, {
-    'budget_phase.budget_phase': 'Audit Outcome',
-    'vote_number.department': 'National Treasury', '_count': 558,
-    'financial_year.financial_year': 2015, 'value.sum': 28690816280
-}, {
-    'budget_phase.budget_phase': 'Final Appropriation',
-    'vote_number.department': 'National Treasury', '_count': 566,
-    'financial_year.financial_year': 2015, 'value.sum': 29005061000
-}, {
-    'budget_phase.budget_phase': 'Main appropriation',
-    'vote_number.department': 'National Treasury', '_count': 685,
-    'financial_year.financial_year': 2015, 'value.sum': 26957304000.0
-}, {
-    'budget_phase.budget_phase': 'Adjusted appropriation',
-    'vote_number.department': 'National Treasury', '_count': 565,
-    'financial_year.financial_year': 2016, 'value.sum': 28471417000
-}, {
-    'budget_phase.budget_phase': 'Audit Outcome',
-    'vote_number.department': 'National Treasury', '_count': 155,
-    'financial_year.financial_year': 2016, 'value.sum': 28199783365
-}, {
-    'budget_phase.budget_phase': 'Final Appropriation',
-    'vote_number.department': 'National Treasury', '_count': 157,
-    'financial_year.financial_year': 2016, 'value.sum': 28471416900
-}, {
-    'budget_phase.budget_phase': 'Main appropriation',
-    'vote_number.department': 'National Treasury', '_count': 684,
-    'financial_year.financial_year': 2016, 'value.sum': 28471417000.0
-}, {
-    'budget_phase.budget_phase': 'Adjusted appropriation',
-    'vote_number.department': 'National Treasury', '_count': 568,
-    'financial_year.financial_year': 2017, 'value.sum': 40584306000
-}, {
-    'budget_phase.budget_phase': 'Audit Outcome',
-    'vote_number.department': 'National Treasury', '_count': 503,
-    'financial_year.financial_year': 2017, 'value.sum': 39792071170
-}, {
-    'budget_phase.budget_phase': 'Final Appropriation',
-    'vote_number.department': 'National Treasury', '_count': 559,
-    'financial_year.financial_year': 2017, 'value.sum': 40484306000
-}, {
-    'budget_phase.budget_phase': 'Main appropriation',
-    'vote_number.department': 'National Treasury', '_count': 820,
-    'financial_year.financial_year': 2017, 'value.sum': 30799220000.0
-}]
-
-test_cells_only_2018 = [{
-    'budget_phase.budget_phase': 'Adjusted appropriation',
-    'vote_number.department': 'National Treasury', '_count': 536,
-    'financial_year.financial_year': 2018, 'value.sum': 29710233000
-}, {
-   'budget_phase.budget_phase': 'Main appropriation',
-   'vote_number.department': 'National Treasury', '_count': 882,
-   'financial_year.financial_year': 2018, 'value.sum': 29358390000.0
-}]
-
-
-test_cells_data_complete = test_cells_data_missing_2018 + test_cells_only_2018
