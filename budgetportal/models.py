b11a616c87d5c45e61cdad91854ac8ecb4dc3fd1
Jaron Rademeyer <jaronrademeyer@gmail.com>
2019-01-30 23:29:13 +0200

    * Remove elaborate, overcomplicated and fairly silly search * Replace it with a simple query

--- budgetportal/models.py
+++ budgetportal/models.py
@@ -300,25 +300,10 @@ class Department(models.Model):
     def _get_latest_department_instance(self):
         """ Try to find the department in the most recent year with the same slug.
         Continue traversing backwards in time until found, or until the original year has been reached. """
-        years = FinancialYear.objects.all().order_by('-slug')
-        original_dept_year = self.government.sphere.financial_year
-        sphere_name = self.government.sphere.name
-        latest_existing_department = None
-        found_or_out_of_years, index = False, 0
-        while not found_or_out_of_years:
-            year = years[index]
-            index += 1
-            if year == original_dept_year:
-                found_or_out_of_years = True
-
-            latest_sphere = Sphere.objects.get(financial_year=year, name=sphere_name)
-            latest_government = latest_sphere.governments.get(name=self.government.name)
-            try:
-                latest_existing_department = latest_government.departments.get(slug=self.slug)
-                found_or_out_of_years = True
-            except Department.DoesNotExist:
-                continue
-        return latest_existing_department
+        newer_departments = Department.objects.filter(government__slug=self.government.slug,
+                                                      government__sphere__slug=self.government.sphere.slug,
+                                                      slug=self.slug).order_by('-government__sphere__financial_year')
+        return newer_departments.first() if newer_departments else None
 
     def _get_financial_year_query(self):
         return '+vocab_financial_years:"%s"' % self.get_financial_year().slug
