6f94e69b914c66de25a5cf20f60aee3098e4541b
Jaron Rademeyer <jaronrademeyer@gmail.com>
2019-02-14 14:25:51 +0200

    * Updated index url

--- budgetportal/urls.py
+++ budgetportal/urls.py
@@ -61,7 +61,7 @@ urlpatterns = [
 
 
     url(r'^datasets.yaml$', cache_page(CACHE_SECS)(views.dataset_category_list)),
-    url(r'^infrastructure-projects/overview$', cache_page(CACHE_SECS)(views.infrastructure_projects_overview)),
+    url(r'^infrastructure-projects$', cache_page(CACHE_SECS)(views.infrastructure_projects_overview)),
     url(r'^infrastructure-projects/(?P<project_slug>[\w-]+)$', cache_page(CACHE_SECS)(views.infrastructure_project_detail)),
     url(r'^datasets'
         '/(?P<category_slug>[\w-]+).yaml$', cache_page(CACHE_SECS)(views.dataset_category)),
