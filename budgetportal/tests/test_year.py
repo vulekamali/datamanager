44a29b6eb8a21ea24573686dd6ac8109f8506371
JD Bothma <jbothma@gmail.com>
2019-05-16 18:50:31 +0200

    Split test data files

--- budgetportal/tests/test_year.py
+++ budgetportal/tests/test_year.py
@@ -10,8 +10,15 @@ import json
 with open('budgetportal/tests/test_data/consolidated_treemap.json', 'r') as CONSOLIDATED_MOCK_DATA:
     CONSOLIDATED_MOCK_DATA = json.load(CONSOLIDATED_MOCK_DATA)
 
-with open('budgetportal/tests/test_data/test_focus_area_pages.json', 'r') as FOCUS_AREA_MOCK_DATA:
-    FOCUS_AREA_MOCK_DATA = json.load(FOCUS_AREA_MOCK_DATA)
+FOCUS_AREA_PROVINCIAL_MOCK_DATA = json.load(
+    open('budgetportal/tests/test_data/test_year_focus_area_pages_provincial.json', 'r')
+)
+FOCUS_AREA_NATIONAL_MOCK_DATA = json.load(
+    open('budgetportal/tests/test_data/test_year_focus_area_pages_national.json', 'r')
+)
+FOCUS_AREA_NATIONAL_SUBPROGRAMMES_MOCK_DATA = json.load(
+    open('budgetportal/tests/test_data/test_year_focus_area_pages_national_subprogrammes.json', 'r')
+)
 
 
 class ConsolidatedTreemapTestCase(TestCase):
@@ -53,7 +60,7 @@ class ConsolidatedTreemapTestCase(TestCase):
 
 
 class FocusAreaPagesTestCase(TestCase):
-    """ Unit tests for the focus area page functions """
+    """ Test  """
 
     def setUp(self):
         self.year = FinancialYear.objects.create(slug="2019-20")
@@ -70,15 +77,19 @@ class FocusAreaPagesTestCase(TestCase):
         mock_dataset.get_openspending_api = Mock(return_value=self.mock_openspending_api)
 
         def get_sphere_dataset(sphere):
-            return {'cells': FOCUS_AREA_MOCK_DATA[sphere]}, self.mock_openspending_api
+            if sphere == "provincial":
+                cells = FOCUS_AREA_PROVINCIAL_MOCK_DATA
+            elif sphere == "national":
+                cells = FOCUS_AREA_NATIONAL_MOCK_DATA
+            return {'cells': cells}, self.mock_openspending_api
 
         self.year.get_focus_area_data = Mock(side_effect=get_sphere_dataset)
         self.year.get_subprogramme_from_actual_and_budgeted_dataset = Mock(
-            return_value={'cells': FOCUS_AREA_MOCK_DATA['national_subprogramme']}
+            return_value={'cells': FOCUS_AREA_NATIONAL_SUBPROGRAMMES_MOCK_DATA}
         )
         self.year.get_expenditure_time_series_dataset = Mock(return_value=mock_dataset)
 
-    def test_provincial_data(self):
+    def test_get_focus_area_preview(self):
         result = self.year.get_focus_area_preview()
         data = result['data']
         self.assertEqual(len(data), 1)
