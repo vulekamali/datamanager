a636abaa7080c95b8b8a1de96feb93a20789d95b
Delena Malan <delenamalan@gmail.com>
2019-05-20 09:33:51 +0200

    Move result to test case

--- budgetportal/tests/test_models.py
+++ budgetportal/tests/test_models.py
@@ -7,7 +7,17 @@ from budgetportal.models import Dataset
 
 class TestDataset(TestCase):
     def setUp(self):
-        self.results = [
+        self.cpi_cpi_query = {
+            'q': '',
+            'fq': ''.join([
+                '+organization:"national-treasury"',
+                '+groups:"cpi-inflation"',
+            ]),
+            'rows': 1000,
+        }
+
+    def test_get_latest_cpi_resource(self):
+        results = [
             {
                 'financial_year': [u'2020-21'],
                 'resources': [{'format': u'CSV',
@@ -20,26 +30,17 @@ class TestDataset(TestCase):
                     {'format': u'CSV', 'id': u'5b315ff0-55e9-4ba8-b88c-2d70093bfe9d'}
                 ]}
         ]
-        self.query = {
-            'q': '',
-            'fq': ''.join([
-                '+organization:"national-treasury"',
-                '+groups:"cpi-inflation"',
-            ]),
-            'rows': 1000,
-        }
-
-    def test_get_latest_cpi_resource(self):
         with patch('budgetportal.models.ckan') as ckan_mock:
             ckan_mock.action.package_search.return_value = {
-                'results': self.results}
+                'results': results}
             cpi_year, cpi_resource_id = Dataset.get_latest_cpi_resource()
-            ckan_mock.action.package_search.assert_called_with(**self.query)
+            ckan_mock.action.package_search.assert_called_with(
+                **self.cpi_cpi_query)
             self.assertEqual('2020-21', cpi_year)
             self.assertEqual(
                 '0c173948-9674-4ca9-aec6-f144bde5cc1e', cpi_resource_id)
 
-    def test_get_latest_cpi_resource_with_multiple_financial_year_values(self):
+    def test_get_latest_cpi_resource_multiple_financial_year_values(self):
         results = [
             {
                 'financial_year': [u'2019-20', '2020-21'],
@@ -52,4 +53,5 @@ class TestDataset(TestCase):
             with self.assertRaises(AssertionError):
                 cpi_year, cpi_resource_id = Dataset.get_latest_cpi_resource()
 
-            ckan_mock.action.package_search.assert_called_with(**self.query)
+            ckan_mock.action.package_search.assert_called_with(
+                **self.cpi_cpi_query)
