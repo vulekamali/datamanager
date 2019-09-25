22b1cf1c5eff78570c16178dc9b10b1b7e683c92
BarisSari <sariba@itu.edu.tr>
2019-09-25 22:03:09 +0300

    new sitemap classes implemented

--- budgetportal/sitemaps.py
+++ budgetportal/sitemaps.py
@@ -2,37 +2,53 @@ from django.contrib import sitemaps
 from django.contrib.sitemaps import GenericSitemap
 from django.urls import reverse
 
-from .models import InfrastructureProjectPart, Department
+from .models import InfrastructureProjectPart, FinancialYear
 from guide_data import category_guides
 
-infrastructure_project_dict = {
-    'queryset': InfrastructureProjectPart.objects.all()
-}
-infrastructure_project_sitemap = GenericSitemap(infrastructure_project_dict, priority=0.6)
+
+class DepartmentListViewSitemap(sitemaps.Sitemap):
+    def items(self):
+        years = []
+        for year in FinancialYear.get_available_years():
+            years.append(str(year.slug))
+
+        return years
+
+    def location(self, item):
+        return reverse('department-list', args=[item])
+
+
+class InfrastructureProjectPartViewSitemap(sitemaps.Sitemap):
+    def items(self):
+        return InfrastructureProjectPart.objects.all()
 
 
 class FocusViewSitemap(sitemaps.Sitemap):
-    priority = 0.5
-    changefreq = 'daily'
-
     def items(self):
-        area = ('health', 'test', 'deneme')
-        return area
+        years = []
+        for year in FinancialYear.get_available_years():
+            years.append(str(year.slug))
 
-    def years(self):
-        years = ('2015-16', '2019-20',)
         return years
 
     def location(self, item):
         # TODO: not working yet
-        # return reverse('focus', kwargs={'financial_year_id': '2019-20', 'focus_slug': 'health'})
-        return reverse('focus', kwargs={'financial_year_id': self.years, 'focus_slug': item})
+        return reverse('focus', kwargs={'financial_year_id':  item, 'focus_slug': 'health'})
+
+
+class FocusJsonViewSitemap(sitemaps.Sitemap):
+    def items(self):
+        years = []
+        for year in FinancialYear.get_available_years():
+            years.append(str(year.slug))
+
+        return years
+
+    def location(self, item):
+        return reverse('focus-json', args=[item])
 
 
 class GuidesViewSitemap(sitemaps.Sitemap):
-    priority = 0.5
-    changefreq = 'daily'
-
     def items(self):
         return category_guides.values()
 
@@ -40,13 +56,23 @@ class GuidesViewSitemap(sitemaps.Sitemap):
         return reverse('guide', args=[item])
 
 
-class StaticViewSitemap(sitemaps.Sitemap):
-    priority = 0.5
-    changefreq = 'daily'
-
+class SearchResultViewSitemap(sitemaps.Sitemap):
     def items(self):
-        return ['about', 'events', 'videos', 'terms-and-conditions', 'resources',
-                'glossary', 'faq', 'dataset-landing-page']
+        years = []
+        for year in FinancialYear.get_available_years():
+            years.append(str(year.slug))
+
+        return years
+
+    def location(self, item):
+        return reverse('search-result', args=[item])
+
+
+class StaticViewSitemap(sitemaps.Sitemap):
+    def items(self):
+        return ['home', 'about', 'events', 'videos', 'terms-and-conditions',
+                'resources', 'glossary', 'faq', 'dataset-landing-page',
+                'guides', 'infrastructure-project-list']
 
     def location(self, item):
         return reverse(item)
@@ -54,7 +80,10 @@ class StaticViewSitemap(sitemaps.Sitemap):
 
 sitemaps = {
     'static': StaticViewSitemap,
-    'infrastructure_projects': infrastructure_project_sitemap,
+    'departments': DepartmentListViewSitemap,
+    'infrastructure_projects': InfrastructureProjectPartViewSitemap,
     'focus': FocusViewSitemap,
+    'focus_json': FocusJsonViewSitemap,
     'guides': GuidesViewSitemap,
+    'search_results': SearchResultViewSitemap,
 }
