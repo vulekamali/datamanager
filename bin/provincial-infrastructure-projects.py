d9a13531eff75113711b7bef826797fe097608e8
Delena Malan <delenamalan@gmail.com>
2019-09-26 14:19:08 +0200

    Include other columns in output csv

--- bin/provincial-infrastructure-projects.py
+++ bin/provincial-infrastructure-projects.py
@@ -1,21 +1,16 @@
 import sys
 from tablib import Databook, Dataset
-
-
-EXTRA_AGENT_HEADER = 'Other parties'
-AGENT_HEADERS = ['Program Implementing Agent',
-                 'Principal Agent',
-                 'Main Contractor']
-HEADERS = AGENT_HEADERS + [EXTRA_AGENT_HEADER]
-
+from irm_report_headers import *
 
 
 class IRMReportSheet(object):
     def __init__(self, data_set):
         self.data_set = data_set
+        self.data_set_dict = data_set.dict
         self.output_data_set = self.create_output_data_set()
         self.contractor_columns = self._get_project_contractor_columns(
             data_set)
+        self.first_contractor_column = self.contractor_columns[0]
 
     def _get_project_contractor_columns(self, data_set):
         headers = data_set.headers
@@ -33,38 +28,59 @@ class IRMReportSheet(object):
 
     def process(self):
         print("Processing data...")
-        for row in self.data_set:
-            self.process_row(row)
+        for i in range(len(self.data_set)):
+            self.process_row(i)
         self.save_to_csv()
 
-    def process_row(self, row):
-        row_dict = {
-            k: [] for k in HEADERS
+    def process_row(self, row_index):
+        row = self.data_set[row_index]
+        if is_empty_row(row):
+            # Don't process empty rows
+            return
+
+        row_contractors = self.get_row_contractors(row)
+        normal_cells = self.get_normal_cells(row_index)
+        self.append_row_to_output_data_set(normal_cells + row_contractors)
+
+    def get_normal_cells(self, row_index):
+        row = self.data_set_dict[row_index]
+        return [row[header] for header in NORMAL_HEADERS]
+
+    def get_row_contractors(self, row):
+        row_contractors = {
+            k: [] for k in AGENT_HEADERS
         }
-        empty_columns = 0
         for col in self.contractor_columns:
             cell = row[col]
-            if not cell or len(cell.strip()) == 0:
-                empty_columns += 1
+            if is_empty_cell(cell):
                 continue
-            for agent_header in AGENT_HEADERS:
+            for agent_header in AGENTS:
                 if cell.lower().strip().startswith(agent_header.lower()):
                     agent_value = cell.split(':')[1].strip()
-                    row_dict[agent_header].append(agent_value)
+                    row_contractors[agent_header].append(agent_value)
                     break
             else:
                 # Agent hasn't been found, append to "Other parties"
-                row_dict[EXTRA_AGENT_HEADER].append(cell)
-        
-        # Don't write empty rows to the output csv
-        if empty_columns == len(self.contractor_columns):
-            return
+                row_contractors[EXTRA_AGENT_HEADER].append(cell)
 
-        self.output_data_set.append([
-            '\n'.join(row_dict[k]) for k in HEADERS
-        ])
+        return [
+            '\n'.join(row_contractors[k]) for k in AGENT_HEADERS
+        ]
+
+    def append_row_to_output_data_set(self, row_data):
+        self.output_data_set.append(row_data)
 
 
+def is_empty_cell(cell):
+    return not cell or (isinstance(cell, basestring) and len(cell.strip()) == 0)
+
+
+def is_not_empty_cell(cell):
+    return not is_empty_cell(cell)
+
+
+def is_empty_row(row):
+    return len(list(filter(is_not_empty_cell, row))) == 0
 
 
 def get_IRM_report_data_set(input_file):
@@ -75,6 +91,7 @@ def get_IRM_report_data_set(input_file):
     sheets = data_book.sheets()
     # TODO: select sheet by name
     return sheets[1]
+
 
 def import_contractor_columns(input_file):
     report_sheet = IRMReportSheet(get_IRM_report_data_set(input_file))
