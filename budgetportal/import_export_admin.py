88984ae93e817a25d489f90b96db5a89c868561d
BarisSari <sariba@itu.edu.tr>
2019-10-12 21:32:26 +0300

    import form and resource classes implemented

--- budgetportal/import_export_admin.py
+++ budgetportal/import_export_admin.py
@@ -1,4 +1,11 @@
-from budgetportal.models import Department, Government, Sphere, InfrastructureProjectPart
+from budgetportal.models import (
+    Department,
+    Government,
+    Sphere,
+    InfrastructureProjectPart,
+    FinancialYear,
+    ProvInfraProject,
+)
 from django import forms, VERSION
 from django.core.exceptions import ValidationError
 from django.db.models import Q, NOT_PROVIDED
@@ -8,7 +15,7 @@ from import_export.admin import ImportForm
 from import_export.fields import Field
 from import_export.formats import base_formats
 from import_export.instance_loaders import ModelInstanceLoader
-from import_export.widgets import Widget
+from import_export.widgets import Widget, ForeignKeyWidget
 import logging
 
 logger = logging.getLogger(__name__)
@@ -52,7 +59,6 @@ class CustomProvinceWidget(Widget):
         )
         value = "".join([province for province in provinces])
         return value
-
 
 
 class CustomGovernmentWidget(Widget):
@@ -204,3 +210,102 @@ class InfrastructureProjectResource(resources.ModelResource):
         if (field.attribute and field.column_name in data) or field.attribute == 'provinces':
             field.save(obj, data, is_m2m)
 
+
+class ProvInfraProjectImportForm(ImportForm):
+    """
+    Form class to use to upload a CSV file to import provincial infrastructure projects.
+    """
+
+    financial_year = forms.ModelChoiceField(
+        queryset=FinancialYear.objects.all(), required=True
+    )
+
+
+class ProvInfraProjectLoader(ModelInstanceLoader):
+    def get_instance(self, row):
+        """
+        Gets a Provincial Infrastructure project instance by IRM_project_id.
+        """
+        project_id = self.resource.fields["IRM_project_id"].clean(row)
+
+        try:
+            return ProvInfraProject.objects.get(IRM_project_id=project_id)
+        except ProvInfraProject.DoesNotExist:
+            pass
+
+        return None
+
+
+class ProvInfraProjectResource(resources.ModelResource):
+    IRM_project_id = Field(attribute="IRM_project_id",
+                           column_name="Project ID")
+    project_number = Field(attribute="project_number",
+                           column_name="Project No.")
+    name = Field(attribute="name", column_name="Project Name")
+    province = Field(attribute="province", column_name="Province")
+    department = Field(attribute="department", column_name="Department")
+    local_municipality = Field(attribute="local_municipality",
+                               column_name="Local Municipality")
+    district_municipality = Field(attribute="district_municipality",
+                                  column_name="District Municipality")
+    latitude = Field(attribute="latitude", column_name="Latitude")
+    longitude = Field(attribute="longitude", column_name="Longitude")
+    status = Field(attribute="status", column_name="Project Status")
+    start_date = Field(attribute="start_date", column_name="Project Start Date")
+    estimated_construction_start_date = Field(attribute="estimated_construction_start_date", column_name="Estimated Construction Start Date")
+    estimated_completion_date = Field(attribute="estimated_completion_date", column_name="Estimated Project Completion Date")
+    contracted_construction_end_date = Field(attribute="contracted_construction_end_date", column_name="Contracted Construction End Date")
+    estimated_constructtion_end_date = Field(attribute="estimated_constructtion_end_date", column_name="Estimated Construction End Date")
+    total_professional_fees = Field(attribute="total_professional_fees", column_name="Professional Fees")
+    total_construction_costs = Field(attribute="total_construction_costs", column_name="Construction Costs")
+    variation_orders = Field(attribute="variation_orders", column_name="Variation Orders")
+    total_project_cost = Field(attribute="total_project_cost", column_name="Total Project Cost")
+    expenditure_from_previous_years_professional_fees = Field(attribute="expenditurre_from_previous_years_professional_fees", column_name="Project Expenditure from Previous Financial Years (Professional Fees)")
+    expenditure_from_previous_years_construction_costs = Field(attribute="expenditurre_from_previous_years_construction_costs", column_name="Project Expenditure from Previous Financial Years (Construction Costs)")
+    expenditure_from_previous_years_total = Field(attribute="expenditurre_from_previous_years_total", column_name="Project Expenditure from Previous Financial Years (TOTAL)")
+    project_expenditure_total = Field(attribute="project_expenditure_tootal", column_name="Project Expenditure (TOTAL)")
+    main_appropriation_professional_fees = Field(attribute="main_appropriation_professional_fees", column_name="Main Budget Appropriation (Professional Fees)")
+    adjustment_appropriation_professional_fees = Field(attribute="adjustment_appropriation_professional_fees", column_name="Adjustment Budget Appropriation (Professional Fees)")
+    main_appropriation_construction_costs = Field(attribute="main_appropriation_construction_costs", column_name="Main Budget Appropriation (Construction Costs)")
+    adjustment_appropriation_construction_costs = Field(attribute="adjustment_appropriation_construction_costs", column_name="Adjustment Budget Appropriation (Construction Costs)")
+    main_appropriation_total = Field(attribute="main_appropriation_total", column_name="Main Budget Appropriation (TOTAL)")
+    adjustment_appropriation_total = Field(attribute="adjustment_appropriation_total", column_name="Adjustment Budget Appropriation (TOTAL)")
+    actual_expenditure_q1 = Field(attribute="actual_expenditure_q1", column_name="Actual Expenditure Q1")
+    actual_expenditure_q2 = Field(attribute="actual_expenditure_q2", column_name="Actual Expenditure Q2")
+    actual_expenditure_q3 = Field(attribute="actual_expenditure_q3", column_name="Actual Expenditure Q3")
+    actual_expenditure_q4 = Field(attribute="actual_expenditure_q4", column_name="Actual Expenditure Q4")
+    budget_programme = Field(attribute="budget_programme", column_name="Budget Programme")
+    primary_funding_source = Field(attribute="primary_funding_source", column_name="Primary Funding Source")
+    nature_of_investment = Field(attribute="nature_of_investment", column_name="Nature of Investment")
+    funding_status = Field(attribute="funding_status", column_name="Funding Status")
+    # program_implementing_agent = Field(attribute="program_implementing_agent", column_name="")
+    # principle_agent = Field(attribute="principle_agent", column_name="")
+    # main_contractor = Field(attribute="main_contractor", column_name="")
+    # other_parties = Field(attribute="other_parties", column_name="")
+    financial_year = Field(
+        attribute="financial_year",
+        column_name="financial_year",
+        widget=ForeignKeyWidget(FinancialYear),
+    )
+
+    class Meta:
+        model = ProvInfraProject
+        exclude = ("id",)
+        instance_loader_class = ProvInfraProjectLoader
+        # import_id_fields = ('IRM_project_id',)
+
+    def before_import_row(self, row, **kwargs):
+        financial_year = self.request.POST.get('financial_year', None)
+        if financial_year:
+            self.request.session['import_context_year'] = financial_year
+        else:
+            try:
+                financial_year = self.request.session['import_context_year']
+            except KeyError as e:
+                raise Exception("Financial year context failure on row import, " +
+                                "check resources.py for more info: {0}".format(e))
+        row["financial_year"] = financial_year
+
+    def __init__(self, request=None, *args, **kwargs):
+        super(ProvInfraProjectResource, self).__init__()
+        self.request = request
