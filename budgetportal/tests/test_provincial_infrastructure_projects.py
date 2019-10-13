f1449a3595d56bd2a6968c3bd084078018ceffd1
BarisSari <sariba@itu.edu.tr>
2019-10-13 21:58:11 +0300

    new asserts added

--- budgetportal/tests/test_provincial_infrastructure_projects.py
+++ budgetportal/tests/test_provincial_infrastructure_projects.py
@@ -49,15 +49,12 @@ class ProvInfraProjectsTestCase(BaseSeleniumTestCase):
         submit_button.click()
 
         selenium.find_element_by_link_text('Prov infra projects').click()
-
         selenium.find_element_by_link_text('IMPORT').click()
 
         file_import = selenium.find_element_by_id('id_import_file')
         financial_year_select = Select(selenium.find_element_by_id('id_financial_year'))
-
         file_import.send_keys(os.path.abspath(filename))
         financial_year_select.select_by_value(str(self.financial_year.id))
-
         selenium.find_element_by_css_selector(
             'input[type="submit"]').click()
 
@@ -71,5 +68,23 @@ class ProvInfraProjectsTestCase(BaseSeleniumTestCase):
         WebDriverWait(selenium, timeout).until(
             lambda driver: selenium.find_element_by_class_name('success'))
 
+        # check values of the first project
         first_project = ProvInfraProject.objects.get(IRM_project_id=30682)
         self.assertEqual(first_project.financial_year.slug, "2019-20")
+        self.assertEqual(first_project.project_number, "W/50042423/WS")
+        self.assertEqual(first_project.name, "BLUE JUNIOR SECONDARY SCHOOL")
+        self.assertEqual(first_project.project_expenditure_total, 1315479)
+
+        # check whether null values are imported as null, and 0 imported as 0
+        self.assertEqual(first_project.total_construction_costs, None)
+        self.assertEqual(first_project.variation_orders, 0)
+
+        # check whether parties/contractor mapping worked correctly
+        self.assertEqual(first_project.program_implementing_agent, "DOPW")
+        self.assertEqual(first_project.other_parties, None)
+
+        # check whether all rows imported (There are 11 rows)
+        count = ProvInfraProject.objects.count()
+        self.assertEqual(count, 11)
+
+
