2af8f50ec838b153e05c3bf13c9ca376f14dc9d5
BarisSari <sariba@itu.edu.tr>
2019-10-16 01:26:05 +0300

    serializer for prov infra project model created

---
+++ budgetportal/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework.serializers import ModelSerializer
+
+from budgetportal.models import ProvInfraProject
+
+
+class ProvInfraProjectSerializer(ModelSerializer):
+    class Meta:
+        model = ProvInfraProject
+        fields = (
+            "project_number",
+            "name",
+            "province",
+            "department",
+            "local_municipality",
+            "district_municipality",
+            "latitude",
+            "longitude",
+            "status",
+            "start_date",
+            "estimated_construction_start_date",
+            "estimated_completion_date",
+            "contracted_construction_end_date",
+            "estimated_construction_end_date",
+            "total_professional_fees",
+            "total_construction_costs",
+            "variation_orders",
+            "total_project_cost",
+            "expenditure_from_previous_years_professional_fees",
+            "expenditure_from_previous_years_construction_costs",
+            "expenditure_from_previous_years_total",
+            "project_expenditure_total",
+            "main_appropriation_professional_fees",
+            "adjustment_appropriation_professional_fees",
+            "main_appropriation_construction_costs",
+            "adjustment_appropriation_construction_costs",
+            "main_appropriation_total",
+            "adjustment_appropriation_total",
+            "actual_expenditure_q1",
+            "actual_expenditure_q2",
+            "actual_expenditure_q3",
+            "actual_expenditure_q4",
+            "budget_programme",
+            "primary_funding_source",
+            "nature_of_investment",
+            "funding_status",
+            "program_implementing_agent",
+            "principle_agent",
+            "main_contractor",
+            "other_parties",
+        )
