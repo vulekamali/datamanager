607248b9d8927a4b0f5471177d5029067c05c848
BarisSari <sariba@itu.edu.tr>
2019-10-16 01:03:22 +0300

    default pagination added

--- budgetportal/settings.py
+++ budgetportal/settings.py
@@ -393,5 +393,7 @@ MARKDOWNIFY_WHITELIST_TAGS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
 }
