f67bd038fec31edafe329071805a946897514c17
JD Bothma <jbothma@gmail.com>
2019-10-15 14:05:27 +0200

    Remove dfaults and Fix a few missing fields

--- budgetportal/webflow/static/js/vulekamali-webflow.js
+++ budgetportal/webflow/static/js/vulekamali-webflow.js
@@ -1,17 +1,23 @@
 (function() {
     if ($("body.provincial-infrastructure-project-detail-page")) {
+        // Project definition
         $(".name-field").html(pageData.project.name);
-        $(".total-project-cost-field").html(pageData.project.total_project_cost);
-        $(".province-name-field").html(pageData.project.province);
-        $(".department-name-field").html(pageData.project.department);
         $(".project-number-field").html(pageData.project.project_number);
+        $(".budget-programme-field").html(pageData.project.budget_programme);
+        $(".nature-of-investment-field").html(pageData.project.nature_of_investment);
+
+        // Administrative details
+        $(".primary-funding-source-field").html(pageData.project.primary_funding_source);
+        $(".funding-status-field").html(pageData.project.funding_status);
+        $(".province-field").html(pageData.project.province);
+        $(".department-field").html(pageData.project.department);
+
+        // Location
         $(".local-municipality-field").html(pageData.project.local_municipality);
         $(".district-municipality-field").html(pageData.project.district_municipality);
         $(".coordinates-field").html(pageData.project.latitude + ", " + pageData.project.longitude);
-        $(".budget-programme-field").html(pageData.project.budget_programme);
-        $(".primary-funding-source-field").html(pageData.project.primary_funding_source);
-        $(".nature-of-investment-field").html(pageData.project.nature_of_investment);
-        $(".funding-status-field").html(pageData.project.funding_status);
+
+        // Implementation
         $(".program-implementing-agent-field").html(pageData.project.program_implementing_agent);
         $(".principle-agent-field").html(pageData.project.principle_agent);
         $(".main-contractor-field").html(pageData.project.main_contractor);
@@ -26,10 +32,10 @@
         $(".estimated-construction-end-date-field").html(pageData.project.estimated_construction_end_date);
 
         // Budgets and spending
+        $(".total-project-cost-field").html(pageData.project.total_project_cost);
         $(".total-professional-fees-field").html(pageData.project.total_professional_fees);
         $(".total-construction-costs-field").html(pageData.project.total_construction_costs);
         $(".variation-orders-field").html(pageData.project.variation_orders);
-        $(".total-project-cost-field").html(pageData.project.total_project_cost);
         $(".expenditure-from-previous-years-professional-fees-field").html(pageData.project.expenditure_from_previous_years_professional_fees);
         $(".expenditure-from-previous-years-construction-costs-field").html(pageData.project.expenditure_from_previous_years_construction_costs);
         $(".expenditure-from-previous-years-total-field").html(pageData.project.expenditure_from_previous_years_total);
@@ -45,6 +51,8 @@
         $(".actual-expenditure-q3-field").html(pageData.project.actual_expenditure_q3);
         $(".actual-expenditure-q4-field").html(pageData.project.actual_expenditure_q4);
 
+        // Maps and visualisations
+        $(".embed-container").css("background-color", "#e1e1e1");
     }
 
 })();
