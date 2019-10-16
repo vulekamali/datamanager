98ce8a41e5feb29667059c13fc51766b52f288d7
BarisSari <sariba@itu.edu.tr>
2019-10-16 12:40:52 +0300

    unused imports removed

--- budgetportal/views.py
+++ budgetportal/views.py
@@ -6,15 +6,11 @@ import urlparse
 import requests
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics
-from rest_framework.filters import SearchFilter
 from slugify import slugify
 
 from budgetportal.csv_gen import generate_csv_response
-from budgetportal.models import Video, Event, FAQ, ProvInfraProject
+from budgetportal.models import Video, Event, FAQ
 from budgetportal.openspending import PAGE_SIZE
-from budgetportal.serializers import ProvInfraProjectSerializer
 from models import FinancialYear, Sphere, Department, InfrastructureProjectPart
 from datasets import Dataset, Category
 from summaries import (
