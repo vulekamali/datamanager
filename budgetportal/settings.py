a3a303b6ac5cc32d1ce90f698434084371799847
JD Bothma <jbothma@gmail.com>
2019-10-29 21:08:31 +0200

    Ignore authorization header while the demo is behind basic auth and I can't get nginx to drop the Authorization header

--- budgetportal/settings.py
+++ budgetportal/settings.py
@@ -388,6 +388,8 @@ MARKDOWNIFY_WHITELIST_TAGS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 20,
