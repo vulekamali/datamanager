aaed14563b565c795d40b0e3b636a9bacac3ba30
JD Bothma <jbothma@gmail.com>
2019-10-29 22:21:02 +0200

    cache provincial search api for 30 minutes

--- budgetportal/webflow/urls.py
+++ budgetportal/webflow/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from . import views
 from rest_framework import routers
 
+
 router = routers.DefaultRouter()
 router.register(
     "infrastructure-projects/provincial/search",
