1afa3456da08f2037b70962363e27c504a69bdc7
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-21 01:56:25 +0300

    test_upload_xlsx_for_prov_infra_projects removed

--- budgetportal/tests/test_prov_infra_projects.py
+++ budgetportal/tests/test_prov_infra_projects.py
@@ -44,57 +44,6 @@ class ProvInfraProjectSeleniumTestCase(BaseSeleniumTestCase):
 
         super(ProvInfraProjectSeleniumTestCase, self).setUp()
 
-    def test_upload_xlsx_for_prov_infra_projects(self):
-        filename = "budgetportal/tests/test_data/test_import_prov_infra_projects.xlsx"
-
-        selenium = self.selenium
-
-        # Login
-        selenium.get("%s%s" % (self.live_server_url, "/admin/"))
-        username = selenium.find_element_by_id("id_login")
-        password = selenium.find_element_by_id("id_password")
-        submit_button = selenium.find_element_by_css_selector('button[type="submit"]')
-        username.send_keys(EMAIL)
-        password.send_keys(PASSWORD)
-        submit_button.click()
-
-        selenium.find_element_by_link_text("Prov infra projects").click()
-        selenium.find_element_by_link_text("IMPORT").click()
-
-        file_import = selenium.find_element_by_id("id_import_file")
-        financial_year_select = Select(selenium.find_element_by_id("id_financial_year"))
-        file_import.send_keys(os.path.abspath(filename))
-        financial_year_select.select_by_value(str(self.financial_year.id))
-        selenium.find_element_by_css_selector('input[type="submit"]').click()
-
-        selenium.implicitly_wait(self.timeout)
-        selenium.find_element_by_name("confirm").click()
-
-        selenium.implicitly_wait(self.timeout)
-        selenium.find_element_by_class_name("success")
-
-        # check values of the first project
-        first_project = ProvInfraProject.objects.get(IRM_project_id=30682)
-        self.assertEqual(first_project.financial_year.slug, "2019-20")
-        self.assertEqual(first_project.project_number, "W/50042423/WS")
-        self.assertEqual(first_project.name, "BLUE JUNIOR SECONDARY SCHOOL")
-        self.assertEqual(first_project.project_expenditure_total, 1315479)
-
-        # check whether null values are imported as null, and 0 imported as 0
-        self.assertEqual(first_project.contracted_construction_end_date, None)
-        self.assertEqual(
-            first_project.expenditure_from_previous_years_construction_costs, None
-        )
-        self.assertEqual(first_project.variation_orders, 0)
-
-        # check whether parties/implementor mapping worked correctly
-        self.assertEqual(first_project.program_implementing_agent, "DOPW")
-        self.assertEqual(first_project.other_parties, None)
-
-        # check whether all rows imported (There are 11 rows)
-        count = ProvInfraProject.objects.count()
-        self.assertEqual(count, 11)
-
     def test_import_irm_snapshot(self):
         # TODO: not completed yet
         filename = "budgetportal/tests/test_data/test_import_prov_infra_projects2.xlsx"
