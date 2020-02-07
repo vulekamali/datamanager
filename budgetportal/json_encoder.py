ac27ef080ce6768d3f4d00362d9d964e1737c507
JD Bothma <jbothma@gmail.com>
2020-02-07 08:00:20 +0200

    Serialise decimals as floats
    This using this data at the moment expect floats, not decimal strings.
    They humanise the numbers before presentation so weirdness will get rounded away.
    
    It might be good to serialise decimals as strings if we provide this in a
    pubic API at some point though, then we can review the best course of action

--- budgetportal/json_encoder.py
+++ budgetportal/json_encoder.py
@@ -6,7 +6,7 @@ import json
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, decimal.Decimal):
-            return str(o)
+            return float(o)
         if isinstance(o, (datetime.date, datetime.datetime)):
             return o.isoformat()
         return json.JSONEncoder.default(self, o)
