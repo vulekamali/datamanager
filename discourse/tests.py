26b31453a60e96514dbc65bbeeea12b7c7c87649
JD Bothma <jbothma@gmail.com>
2020-02-24 16:54:14 +0200

    Finish test and fix string/bytestring en/decoding

--- discourse/tests.py
+++ discourse/tests.py
@@ -14,13 +14,12 @@ class SSOTestCase(TestCase):
         """
         Test with example at
         https://meta.discourse.org/t/official-single-sign-on-for-discourse-sso/13045
+        EXCEPT response payload and thus signature are different because of different user data.
         """
         with self.settings(
             DISCOURSE_SSO_SECRET="d836444a9e4084d5b224a60c208dce14",
             DISCOURSE_SSO_URLS={
-                "somessoclient": {
-                    "DISCOURSE_SSO_SECRET": "http://www.example.com/discourse/sso"
-                }
+                "somessoclient": "http://discuss.example.com/session/sso_login"
             },
         ):
             params = {
@@ -29,11 +28,10 @@ class SSOTestCase(TestCase):
             }
             response = self.client.get("/somessoclient/sso", params)
             redirect_url = (
-                "http://www.example.com/discourse/sso?"
-                "sso=bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGI%3D%0A&"
-                "sig=2828aa29899722b35a2f191d34ef9b3ce695e0e6eeec47deb46d588d70c7cb56"
+                "http://discuss.example.com/session/sso_login?"
+                "sso=bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGImZW1haWw9YWRtaW4lNDBsb2Nh%0AbGhvc3QmZXh0ZXJuYWxfaWQ9MSZ1c2VybmFtZT1hZG1pbiU0MGxvY2FsaG9zdCZuYW1lPQ%3D%3D%0A&"
+                "sig=314288361dd37ffd54f61b620a5be30607fe05836318611a8d448d7c18375554"
             )
-            print(response.content)
             self.assertRedirects(
                 response, redirect_url, status_code=302, fetch_redirect_response=False
             )
