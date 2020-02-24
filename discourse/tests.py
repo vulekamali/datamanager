f81a60046261d1cd3ab9e2df7d9efcb364ac1ef2
JD Bothma <jbothma@gmail.com>
2020-02-24 16:15:46 +0200

    Add failing test

--- discourse/tests.py
+++ discourse/tests.py
@@ -3,4 +3,36 @@
 
 from django.test import TestCase
 
-# Create your tests here.
+
+class SSOTestCase(TestCase):
+    fixtures = ["development-first-user"]
+
+    def setUp(self):
+        self.client.login(username="admin@localhost", password="password")
+
+    def test_valid_request(self):
+        """
+        Test with example at
+        https://meta.discourse.org/t/official-single-sign-on-for-discourse-sso/13045
+        """
+        with self.settings(
+            DISCOURSE_SSO_SECRET="d836444a9e4084d5b224a60c208dce14",
+            DISCOURSE_SSO_URLS={
+                "somessoclient": {
+                    "DISCOURSE_SSO_SECRET": "http://www.example.com/discourse/sso"
+                }
+            },
+        ):
+            params = {
+                "payload": "bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGI=\n",
+                "signature": "2828aa29899722b35a2f191d34ef9b3ce695e0e6eeec47deb46d588d70c7cb56",
+            }
+            response = self.client.get("/somessoclient/sso", params)
+            redirect_url = (
+                "http://www.example.com/discourse/sso?"
+                "sso=bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGI%3D%0A&"
+                "sig=2828aa29899722b35a2f191d34ef9b3ce695e0e6eeec47deb46d588d70c7cb56"
+            )
+            self.assertRedirects(
+                response, redirect_url, status_code=302, fetch_redirect_response=False
+            )
