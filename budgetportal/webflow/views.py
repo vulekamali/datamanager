3393a1af08b81283e9df65570ba642056fb77522
Tomasz Kołek <tomaszkolek0@gmail.com>
2020-03-04 15:27:26 +0100

    blacken.

--- budgetportal/webflow/views.py
+++ budgetportal/webflow/views.py
@@ -92,7 +92,6 @@ class ProvInfraProjectCSVGeneratorMixIn:
             yield writer.writerow(row)
 
 
-
 class ProvInfaProjectCSVSnapshotSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProvInfraProjectSnapshot
