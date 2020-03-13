04cdf07042e8dbfd296c3aed694945eaf094505c
Tomasz Kołek <tomaszkolek0@gmail.com>
2020-03-13 19:16:23 +0100

    blacken

--- budgetportal/tests/helpers.py
+++ budgetportal/tests/helpers.py
@@ -58,17 +58,22 @@ class BaseSeleniumTestCase(LiveServerTestCase):
         for db_name in self._databases_names(include_mirrors=False):
             # Flush the database
             inhibit_post_migrate = (
-                    self.available_apps is not None or
-                    (  # Inhibit the post_migrate signal when using serialized
-                        # rollback to avoid trying to recreate the serialized data.
-                            self.serialized_rollback and
-                            hasattr(connections[db_name], '_test_serialized_contents')
-                    )
+                self.available_apps is not None
+                or (  # Inhibit the post_migrate signal when using serialized
+                    # rollback to avoid trying to recreate the serialized data.
+                    self.serialized_rollback
+                    and hasattr(connections[db_name], "_test_serialized_contents")
+                )
             )
-            call_command('flush', verbosity=0, interactive=False,
-                         database=db_name, reset_sequences=False,
-                         allow_cascade=True,
-                         inhibit_post_migrate=inhibit_post_migrate)
+            call_command(
+                "flush",
+                verbosity=0,
+                interactive=False,
+                database=db_name,
+                reset_sequences=False,
+                allow_cascade=True,
+                inhibit_post_migrate=inhibit_post_migrate,
+            )
 
 
 class CustomLiveServerTestCase(LiveServerTestCase):
@@ -78,14 +83,19 @@ class CustomLiveServerTestCase(LiveServerTestCase):
         for db_name in self._databases_names(include_mirrors=False):
             # Flush the database
             inhibit_post_migrate = (
-                    self.available_apps is not None or
-                    (  # Inhibit the post_migrate signal when using serialized
-                        # rollback to avoid trying to recreate the serialized data.
-                            self.serialized_rollback and
-                            hasattr(connections[db_name], '_test_serialized_contents')
-                    )
+                self.available_apps is not None
+                or (  # Inhibit the post_migrate signal when using serialized
+                    # rollback to avoid trying to recreate the serialized data.
+                    self.serialized_rollback
+                    and hasattr(connections[db_name], "_test_serialized_contents")
+                )
             )
-            call_command('flush', verbosity=0, interactive=False,
-                         database=db_name, reset_sequences=False,
-                         allow_cascade=True,
-                         inhibit_post_migrate=inhibit_post_migrate)
+            call_command(
+                "flush",
+                verbosity=0,
+                interactive=False,
+                database=db_name,
+                reset_sequences=False,
+                allow_cascade=True,
+                inhibit_post_migrate=inhibit_post_migrate,
+            )
