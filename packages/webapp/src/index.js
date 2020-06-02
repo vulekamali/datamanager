60c43f1821af3f8a96a79af9d2a35fa55b2b55d8
JD Bothma <jbothma@gmail.com>
2020-06-02 08:42:51 +0200

    Remove hardcoded budget resource list on homepage

--- packages/webapp/src/index.js
+++ packages/webapp/src/index.js
@@ -1,9 +1,9 @@
 import 'core-js/fn/object/assign';
 import 'core-js/fn/array/find';
 import 'core-js/fn/promise';
-import 'core-js/fn/weak-set'; 
+import 'core-js/fn/weak-set';
 import 'core-js/fn/weak-map';
-import 'core-js/fn/set'; 
+import 'core-js/fn/set';
 import 'core-js/fn/map';
 
 import './adaptors/homepage-hero';
@@ -13,4 +13,3 @@ import './adaptors/provincial-treemap';
 import './adaptors/preview-pages';
 import './adaptors/consolidated-treemap';
 import './adaptors/focus-areas-preview';
-import './adaptors/resources-homepage';
