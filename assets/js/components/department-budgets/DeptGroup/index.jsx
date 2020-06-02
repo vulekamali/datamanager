cb775be4e3494927e4a5728e0c842267415c485d
JD Bothma <jbothma@gmail.com>
2020-06-02 20:18:51 +0200

    Make resources show whether departments exist yet or not.

--- assets/js/components/department-budgets/DeptGroup/index.jsx
+++ assets/js/components/department-budgets/DeptGroup/index.jsx
@@ -3,45 +3,46 @@ import GovernmentResources from './../../GovernmentResources/GovernmentResources
 
 import Map from './partials/Map.jsx';
 
-export default function DeptGroup({ map, linksArray, name: title, doubleRow, empty }) {
+function notAvailableMessage() {
+  return (
+    <div>
+      <p className="u-fontStyle u-fontStyle--italic">This data is not yet available. Provincial budgets are tabled after the national budget has been announced. This is because the national budget determines the amount of money each province receives. We expect to be able make provincial budget data available by April {(new Date()).getFullYear()}.</p>
+      <p>{"In the meantime you view previous financial years' data by selecting a year at the top of your screen."}</p>
+    </div>
+  );
+}
 
-  if (empty) {
-    return (
+function departments(linksArray, doubleRow) {
+  return (
+    <ul className={`DeptGroup-list${doubleRow ? ' DeptGroup-list--doubleRow' : ''}`}>
+      {
+        linksArray.map(({ name, url_path: url }) => {
+          return (
+            <li className="DeptGroup-item">
+              <a className="DeptGroup-link" href={url} dangerouslySetInnerHTML={{ __html: name }} />
+            </li>
+          );
+        })
+      }
+    </ul>
+  )
+}
+
+export default function DeptGroup({ map, linksArray, label, name, doubleRow, empty }) {
+  return (
+    <div>
       <div className="DeptGroup">
         <div className="DeptGroup-wrap">
-          <h3 className="DeptGroup-title">{title} Department Budgets</h3>
-          <p className="u-fontStyle u-fontStyle--italic">This data is not yet available. Provincial budgets are tabled after the national budget has been announced. This is because the national budget determines the amount of money each province receives. We expect to be able make provincial budget data available by April {(new Date()).getFullYear()}.</p>
-          <p>{"In the meantime you view previous financial years' data by selecting a year at the top of your screen."}</p>
+          <h3 className="DeptGroup-title">{label} Department Budgets</h3>
+          {empty ? notAvailableMessage() : departments(linksArray, doubleRow)}
         </div>
-        <div className="DeptGroup-map">
-          {Map(map)}
-        </div>
+          <div className="DeptGroup-map">
+            {Map(map)}
+          </div>
+          <div>{"hello there"}</div>
+          <GovernmentResources title={label} />
+          <div>{"bye now"}</div>
       </div>
-    );
-  }
-
-  return (
-    <div className="DeptGroup">
-      <div className="DeptGroup-wrap">
-        <h3 className="DeptGroup-title">{title} Department Budgets</h3>
-        <ul className={`DeptGroup-list${doubleRow ? ' DeptGroup-list--doubleRow' : ''}`}>
-          {
-            linksArray.map(({ name, url_path: url }) => {
-              return (
-                <li className="DeptGroup-item">
-                  <a className="DeptGroup-link" href={url} dangerouslySetInnerHTML={{ __html: name }} />
-                </li>
-              );
-            })
-          }
-        </ul>
-      </div>
-      <div className="DeptGroup-map">
-        {Map(map)}
-      </div>
-      <div>{"hello there"}</div>
-      <GovernmentResources title={title} />
-      <div>{"bye now"}</div>
     </div>
   );
 }
