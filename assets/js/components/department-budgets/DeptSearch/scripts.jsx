7bf2ab88e321d8682a3b6057402229ac545364dc
JD Bothma <jbothma@gmail.com>
2020-06-04 16:00:59 +0200

    Factor out resources code to own module

--- assets/js/components/department-budgets/DeptSearch/scripts.jsx
+++ assets/js/components/department-budgets/DeptSearch/scripts.jsx
@@ -4,39 +4,7 @@ import updateQs from './../../../utilities/js/helpers/updateQs.js';
 import { DeptSearch, makeGroups } from './index.jsx';
 import filterResults from './partials/filterResults.js';
 import fetchWrapper from './../../../utilities/js/helpers/fetchWrapper.js';
-
-const originalBudgetGroups = [
-  'estimates-of-provincial-revenue-and-expenditure',
-  'people-s-guides',
-  'occasional-budget-documents',
-  'tax-pocket-guides',
-  'appropriation-bills',
-  'budget-highlights',
-  'budget-reviews',
-  'budget-speeches',
-  'division-of-revenue-bills',
-  'estimates-of-national-expenditure',
-];
-const adjustedBudgetGroups = [
-  'adjusted-estimates-of-national-expenditure',
-  'adjusted-estimates-of-provincial-revenue-and-expenditure',
-  'division-of-revenue-amendment-bills',
-  'adjustments-appropriation-bills',
-  'medium-term-budget-policy-statements',
-];
-
-const governments = [
-  "South Africa",
-  "Eastern Cape",
-  "Free State",
-  "Gauteng",
-  "KwaZulu-Natal",
-  "Limpopo",
-  "Mpumalanga",
-  "North West",
-  "Northern Cape",
-  "Western Cape",
-];
+import { resourcesUrl, resultsToResources, initialResourceGroups } from './../../GovernmentResources/governmentResourcesData.js';
 
 class DeptSearchContainer extends Component {
   constructor(props) {
@@ -113,73 +81,6 @@ class DeptSearchContainer extends Component {
   }
 }
 
-function initialResourceGroups() {
-  return initialiseResourceGroups(() => null);
-}
-function emptyResourceGroups() {
-  return initialiseResourceGroups(() => []);
-}
-function initialiseResourceGroups(valueFunction) {
-  return governments.reduce((groups, gov) => {
-    groups[gov] = {"original": valueFunction(), "adjusted": valueFunction()};
-    return groups;
-  }, {});
-}
-
-function resourcesUrl(ckanUrl, financialYear) {
-  const groupsClause = [...originalBudgetGroups, ...adjustedBudgetGroups]
-        .map(g => `groups:"${g}"`)
-        .join(' OR ');
-  const fqParams = [
-    'organization:"national-treasury"',
-    `(${groupsClause})`,
-    `vocab_financial_years:"${financialYear}"`,
-  ].join('+');
-  const searchParams = new URLSearchParams();
-  searchParams.set('q', '');
-  searchParams.set('fq', fqParams);
-  searchParams.set('rows', '1000');
-  return `${ckanUrl}/api/3/action/package_search?${searchParams.toString()}`
-}
-
-/**
- * @param {array} results - CKAN package_search action results field: an array of packages.
-
- * Returns an object keyed by government name, where the values are objects
- * with that government's budgetResources and adjustedBudgetResources as keys.
- */
-function resultsToResources(results) {
-  return results.reduce(datasetReducer, emptyResourceGroups());
-}
-
-function datasetReducer(governments, dataset) {
-  const governmentName = datasetGovernment(dataset);
-  if (governmentName === null) {
-    console.warn("Unknown government", dataset);
-    return governments;
-  }
-  const government = governments[governmentName];
-  if (dataset.groups.some(group => originalBudgetGroups.includes(group.name)))
-    government.original.push(...(dataset.resources));
-  if (dataset.groups.some(group => adjustedBudgetGroups.includes(group.name)))
-    government.adjusted.push(...(dataset.resources));
-  return governments;
-}
-
-function datasetGovernment(dataset) {
-  if (dataset.sphere.length > 1) {
-    console.error("More than one sphere", dataset);
-    return null;
-  }
-  if (dataset.sphere[0] === "national")
-      return "South Africa";
-  if (dataset.sphere[0] === "provincial") {
-    if (dataset.province.length > 1)
-      console.error("More than one province", dataset);
-    return dataset.province[0];
-  }
-  return null;
-}
 
 function scripts() {
   const componentsList = document.getElementsByClassName('js-initDeptSearch');
