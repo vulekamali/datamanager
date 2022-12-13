011b9395245efab4437dab81f1ef5e82c8cb1bba
emreyildirim2038@gmail.com <emreyildirim2038@gmail.com>
2022-12-13 23:18:28 +0300

    frictionless validate

--- performance/admin.py
+++ performance/admin.py
@@ -4,6 +4,8 @@ from io import StringIO
 from performance import models
 from django_q.tasks import async_task
 
+from frictionless import validate
+
 import os
 import json
 import threading
@@ -16,24 +18,28 @@ def get_key_value_or_default(fields, key):
     else:
         return ""
 
-VALID_REPORT_TYPES = ['Provincial Institutions Oversight Performance  Report','National Institutions Oversight Performance  Report']
 
-def generate_import_report(report_type_validated, not_matching_departments, primary_key_violations):
+VALID_REPORT_TYPES = ['Provincial Institutions Oversight Performance  Report',
+                      'National Institutions Oversight Performance  Report']
+
+
+def generate_import_report(report_type_validated, frictionless_report, not_matching_departments):
     report = ""
     if not report_type_validated:
-        report += 'Report preamble must be for one of '+ os.linesep
+        report += "Report preamble must be for one of " + os.linesep
         for report_type in VALID_REPORT_TYPES:
-            report += f"* {report_type}" + os.linesep
+            report += f"* {report_type} {os.linesep}"
+
+    if frictionless_report:
+        if not frictionless_report.valid:
+            for error in frictionless_report.tasks[0].errors:
+                report += f"* {error.message} {os.linesep}"
 
     if len(not_matching_departments) > 0:
         report += "Department names that could not be matched on import : " + os.linesep
         for department in not_matching_departments:
             report += f"* {department} {os.linesep}"
 
-    if len(primary_key_violations) > 0:
-        report += "Cannot create multiple elements with the same department & indicator names : " + os.linesep
-        for violation in primary_key_violations:
-            report += f"* department name : {violation[0]} / indicator : {violation[1]}" + os.linesep
     return report
 
 
@@ -42,7 +48,6 @@ def save_imported_indicators(parsed_data, obj_id):
     num_imported = 0
     total_record_count = len(parsed_data)
     not_matching_departments = []
-    primary_key_violations = []
 
     for department in report_departments:
         department_obj = models.Department.objects.filter(name=department).first()
@@ -50,87 +55,80 @@ def save_imported_indicators(parsed_data, obj_id):
 
     for indicator_data in parsed_data:
         department_name = indicator_data[1]
-        indicator_name = indicator_data[6]
         department_obj = models.Department.objects.filter(name=department_name).first()
-        existing_obj = models.Indicator.objects.filter(department=department_obj, indicator_name=indicator_name)
 
-        if len(existing_obj) <= 0:
-            if department_obj:
-                models.Indicator.objects.create(
-                    indicator_name=indicator_data[6],
-                    department=department_obj,
-                    source_id=obj_id,
+        if department_obj:
+            models.Indicator.objects.create(
+                indicator_name=indicator_data[6],
+                department=department_obj,
+                source_id=obj_id,
 
-                    q1_target=indicator_data[11],
-                    q1_actual_output=indicator_data[12],
-                    q1_deviation_reason=indicator_data[13],
-                    q1_corrective_action=indicator_data[14],
-                    q1_national_comments=indicator_data[16],
-                    q1_otp_comments=indicator_data[15],
-                    q1_dpme_coordinator_comments='',
-                    q1_treasury_comments='',
+                q1_target=indicator_data[11],
+                q1_actual_output=indicator_data[12],
+                q1_deviation_reason=indicator_data[13],
+                q1_corrective_action=indicator_data[14],
+                q1_national_comments=indicator_data[16],
+                q1_otp_comments=indicator_data[15],
+                q1_dpme_coordinator_comments='',
+                q1_treasury_comments='',
 
-                    q2_target=indicator_data[17],
-                    q2_actual_output=indicator_data[18],
-                    q2_deviation_reason=indicator_data[19],
-                    q2_corrective_action=indicator_data[20],
-                    q2_national_comments=indicator_data[22],
-                    q2_otp_comments=indicator_data[21],
-                    q2_dpme_coordinator_comments='',
-                    q2_treasury_comments='',
+                q2_target=indicator_data[17],
+                q2_actual_output=indicator_data[18],
+                q2_deviation_reason=indicator_data[19],
+                q2_corrective_action=indicator_data[20],
+                q2_national_comments=indicator_data[22],
+                q2_otp_comments=indicator_data[21],
+                q2_dpme_coordinator_comments='',
+                q2_treasury_comments='',
 
-                    q3_target=indicator_data[23],
-                    q3_actual_output=indicator_data[24],
-                    q3_deviation_reason=indicator_data[25],
-                    q3_corrective_action=indicator_data[26],
-                    q3_national_comments=indicator_data[28],
-                    q3_otp_comments=indicator_data[27],
-                    q3_dpme_coordinator_comments='',
-                    q3_treasury_comments='',
+                q3_target=indicator_data[23],
+                q3_actual_output=indicator_data[24],
+                q3_deviation_reason=indicator_data[25],
+                q3_corrective_action=indicator_data[26],
+                q3_national_comments=indicator_data[28],
+                q3_otp_comments=indicator_data[27],
+                q3_dpme_coordinator_comments='',
+                q3_treasury_comments='',
 
-                    q4_target=indicator_data[29],
-                    q4_actual_output=indicator_data[30],
-                    q4_deviation_reason=indicator_data[31],
-                    q4_corrective_action=indicator_data[32],
-                    q4_national_comments=indicator_data[34],
-                    q4_otp_comments=indicator_data[33],
-                    q4_dpme_coordinator_comments='',
-                    q4_treasury_comments='',
+                q4_target=indicator_data[29],
+                q4_actual_output=indicator_data[30],
+                q4_deviation_reason=indicator_data[31],
+                q4_corrective_action=indicator_data[32],
+                q4_national_comments=indicator_data[34],
+                q4_otp_comments=indicator_data[33],
+                q4_dpme_coordinator_comments='',
+                q4_treasury_comments='',
 
-                    annual_target=indicator_data[35],
-                    annual_aggregate_output='',
-                    annual_pre_audit_output=indicator_data[37],
-                    annual_deviation_reason=indicator_data[38],
-                    annual_corrective_action=indicator_data[39],
-                    annual_otp_comments=indicator_data[40],
-                    annual_national_comments=indicator_data[41],
-                    annual_dpme_coordincator_comments='',
-                    annual_treasury_comments='',
-                    annual_audited_output=indicator_data[42],
+                annual_target=indicator_data[35],
+                annual_aggregate_output='',
+                annual_pre_audit_output=indicator_data[37],
+                annual_deviation_reason=indicator_data[38],
+                annual_corrective_action=indicator_data[39],
+                annual_otp_comments=indicator_data[40],
+                annual_national_comments=indicator_data[41],
+                annual_dpme_coordincator_comments='',
+                annual_treasury_comments='',
+                annual_audited_output=indicator_data[42],
 
-                    sector=indicator_data[0],
-                    programme_name=indicator_data[2],
-                    subprogramme_name=indicator_data[3],
-                    frequency=indicator_data[5],
-                    type=indicator_data[7],
-                    subtype=indicator_data[8],
-                    mtsf_outcome=indicator_data[9],
-                    cluster=indicator_data[10],
-                    uid=indicator_data[43],
-                )
-                num_imported = num_imported + 1
-            elif department_name not in not_matching_departments:
-                not_matching_departments.append(department_name)
-        else:
-            violation = [existing_obj[0].department.name, existing_obj[0].indicator_name]
-            if violation not in primary_key_violations:
-                primary_key_violations.append(violation)
+                sector=indicator_data[0],
+                programme_name=indicator_data[2],
+                subprogramme_name=indicator_data[3],
+                frequency=indicator_data[5],
+                type=indicator_data[7],
+                subtype=indicator_data[8],
+                mtsf_outcome=indicator_data[9],
+                cluster=indicator_data[10],
+                uid=indicator_data[43],
+            )
+            num_imported = num_imported + 1
+        elif department_name not in not_matching_departments:
+            not_matching_departments.append(department_name)
 
     obj_to_update = models.EQPRSFileUpload.objects.get(id=obj_id)
 
     obj_to_update.num_imported = num_imported
     obj_to_update.num_not_imported = total_record_count - num_imported
-    obj_to_update.import_report = generate_import_report(True, not_matching_departments, primary_key_violations)
+    obj_to_update.import_report = generate_import_report(True, None, not_matching_departments)
     obj_to_update.save()
 
 
@@ -141,7 +139,22 @@ def validate_report_type(full_text, obj_id):
 
     if not validated:
         obj_to_update = models.EQPRSFileUpload.objects.get(id=obj_id)
-        obj_to_update.import_report = generate_import_report(False, [], [])
+        obj_to_update.num_imported = None
+        obj_to_update.num_not_imported = None
+        obj_to_update.import_report = generate_import_report(False, None, [])
+        obj_to_update.save()
+
+    return validated
+
+
+def validate_frictionless(data, obj_id):
+    report = validate(data)
+    validated = report.valid
+    if not validated:
+        obj_to_update = models.EQPRSFileUpload.objects.get(id=obj_id)
+        obj_to_update.num_imported = None
+        obj_to_update.num_not_imported = None
+        obj_to_update.import_report = generate_import_report(True, report, [])
         obj_to_update.save()
 
     return validated
@@ -189,9 +202,12 @@ class EQPRSFileUploadAdmin(admin.ModelAdmin):
             reader = csv.reader(f, delimiter=',')
             parsed_data = [tuple(row) for row in reader]
             parsed_data = [x for x in parsed_data if x]
-            del parsed_data[0:3]
+            del parsed_data[0:2]  # delete first 3 rows
 
-            threading.Thread(target=save_imported_indicators, args=(parsed_data, obj.id)).start()
+            frictionless_validated = validate_frictionless(parsed_data, obj.id)
+            if frictionless_validated:
+                del parsed_data[0]  # delete header row
+                threading.Thread(target=save_imported_indicators, args=(parsed_data, obj.id)).start()
 
     def success(self, obj):
         if obj.task:
