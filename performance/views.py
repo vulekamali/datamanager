b985e66033d1b9a96d5f8d18c8c9920ad3b62bf5
mahlatse <mtmolotom@yahoo.com>
2023-01-12 12:47:29 +0200

    add filters  code

--- performance/views.py
+++ performance/views.py
@@ -3,13 +3,28 @@ from .models import Indicator
 from .serializer import IndicatorSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import viewsets
-
+from django_filters import rest_framework as filters
 # Create your views here.
+
+class IndicatorFilter(filters.FilterSet):
+    financial_year = filters.CharFilter(lookup_expr= 'icontains')
+    government_name = filters.CharFilter(lookup_expr= 'icontains')
+    sphere_name = filters.CharFilter(lookup_expr= 'icontains')
+    frequency = filters.CharFilter(lookup_expr= 'icontains')
+
+    class Meta:
+        model = Indicator
+        filter_fields = ['department_name','financial_year', 'government_name','sphere_name','frequency']
 
 class IndicatorReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = IndicatorSerializer
+    filterset_class = IndicatorFilter
     queryset = Indicator.objects.all()
     http_method_names = ['get','head']
+
+
+
+    
     
 
 
