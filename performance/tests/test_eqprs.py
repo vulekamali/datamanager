0b0cc064be526d2ba789e9b311d51e5519589053
emreyildirim2038@gmail.com <emreyildirim2038@gmail.com>
2023-01-12 15:15:26 +0300

    asserting all columns

--- performance/tests/test_eqprs.py
+++ performance/tests/test_eqprs.py
@@ -68,6 +68,7 @@ class EQPRSFileUploadTestCase(TestCase):
         )
         performance.admin.save_imported_indicators(test_element.id)
         test_element.refresh_from_db()
+        assert test_element.num_not_imported == 2
         assert test_element.import_report == """Department names that could not be matched on import : 
 * Health 
 """
@@ -91,3 +92,57 @@ class EQPRSFileUploadTestCase(TestCase):
         assert test_element.num_imported == 2
         assert indicator.indicator_name == "9.1.2 Number of statutory documents tabled at Legislature"
         assert indicator.sector == "Health"
+        assert indicator.programme_name == "Programme 1: Administration"
+        assert indicator.subprogramme_name == "Sub-Programme 1.1: Office of the MEC"
+        assert indicator.frequency == "quarterly"
+        assert indicator.type =="Non-Standardized"
+        assert indicator.subtype=="Max"
+        assert indicator.mtsf_outcome=="Priority 3: Education, Skills And Health"
+        assert indicator.cluster=="The Social Protection, Community and Human Development cluster"
+
+        assert indicator.q1_target == "0"
+        assert indicator.q1_actual_output == "0"
+        assert indicator.q1_deviation_reason == "There is no target for quarter one"
+        assert indicator.q1_corrective_action == ""
+        assert indicator.q1_national_comments == ""
+        assert indicator.q1_otp_comments == ""
+        assert indicator.q1_dpme_coordinator_comments == ""
+        assert indicator.q1_treasury_comments == ""
+
+        assert indicator.q2_target == "1"
+        assert indicator.q2_actual_output == "2"
+        assert indicator.q2_deviation_reason == "Target achieved"
+        assert indicator.q2_corrective_action == ""
+        assert indicator.q2_national_comments == ""
+        assert indicator.q2_otp_comments == ""
+        assert indicator.q2_dpme_coordinator_comments == ""
+        assert indicator.q2_treasury_comments == ""
+
+        assert indicator.q3_target == "2"
+        assert indicator.q3_actual_output == "2"
+        assert indicator.q3_deviation_reason == "Target achieved"
+        assert indicator.q3_corrective_action == ""
+        assert indicator.q3_national_comments == ""
+        assert indicator.q3_otp_comments == ""
+        assert indicator.q3_dpme_coordinator_comments == ""
+        assert indicator.q3_treasury_comments == ""
+
+        assert indicator.q4_target == "5"
+        assert indicator.q4_actual_output == "8"
+        assert indicator.q4_deviation_reason == "All statutory documents submitted."
+        assert indicator.q4_corrective_action == ""
+        assert indicator.q4_national_comments == ""
+        assert indicator.q4_otp_comments == ""
+        assert indicator.q4_dpme_coordinator_comments == ""
+        assert indicator.q4_treasury_comments == ""
+
+        assert indicator.annual_target == "8"
+        assert indicator.annual_aggregate_output == ""
+        assert indicator.annual_pre_audit_output == "8"
+        assert indicator.annual_deviation_reason == "Target achieved "
+        assert indicator.annual_corrective_action == ""
+        assert indicator.annual_otp_comments == ""
+        assert indicator.annual_national_comments == ""
+        assert indicator.annual_dpme_coordincator_comments == ""
+        assert indicator.annual_treasury_comments == ""
+        assert indicator.annual_audited_output == ""
