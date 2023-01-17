b53b2a2f2098c0ca2dfbcdd1118f48995b32ff58
emreyildirim2038@gmail.com <emreyildirim2038@gmail.com>
2023-01-17 16:01:50 +0300

    before mui

--- assets/js/components/performance/Table/scripts.jsx
+++ assets/js/components/performance/Table/scripts.jsx
@@ -1,5 +1,22 @@
+import {Component, render, h} from "preact";
+
+class TableContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>test</div>
+    }
+}
+
 function scripts() {
-    console.log('scripts')
+    const parent = document.getElementsByClassName('js-initTabularView')[0];
+
+    render(
+        <TableContainer/>,
+        parent
+    );
 }
 
 
