a8906bf08e6435670c89f94af6f05672b869105a
emreyildirim2038@gmail.com <emreyildirim2038@gmail.com>
2023-01-27 10:25:13 +0300

    added all filters

--- assets/js/components/performance/Table/scripts.jsx
+++ assets/js/components/performance/Table/scripts.jsx
@@ -23,6 +23,10 @@ class TabularView extends Component {
             departments: null,
             financialYears: null,
             frequencies: null,
+            governments: null,
+            mtsfOutcomes: null,
+            sectors: null,
+            spheres: null,
             selectedFilters: {}
         }
     }
@@ -41,20 +45,22 @@ class TabularView extends Component {
             if (value !== '') {
                 url += firstParam ? '?' : '&';
                 firstParam = false;
-                url += `${key}=${value}`;
+                url += `${key}=${encodeURI(value)}`;
             }
         })
-        console.log({url})
 
         fetchWrapper(url)
             .then((response) => {
-                console.log({response})
                 this.setState({
                     ...this.state,
                     rows: response.results.items,
                     departments: response.results.facets['department_name'],
                     financialYears: response.results.facets['financial_year_slug'],
                     frequencies: response.results.facets['frequency'],
+                    governments: response.results.facets['government_name'],
+                    mtsfOutcomes: response.results.facets['mtsf_outcome'],
+                    sectors: response.results.facets['sector'],
+                    spheres: response.results.facets['sphere_name'],
                 });
             })
             .catch((errorResult) => console.warn(errorResult));
@@ -63,8 +69,9 @@ class TabularView extends Component {
     renderTableHead() {
         return (
             <TableRow>
-                {Object.keys(this.state.rows[0]).map((key) => {
+                {Object.keys(this.state.rows[0]).map((key, index) => {
                     return (<TableCell
+                        key={index}
                         style={{borderRight: '1px solid #c6c6c6'}}
                     ><b>{key}</b></TableCell>)
                 })}
@@ -72,15 +79,17 @@ class TabularView extends Component {
         )
     }
 
-    renderTableCells(row, isAlternating) {
+    renderTableCells(row, index) {
+        const isAlternating = index % 2 !== 0;
         return (
             <TableRow
-                key={row.name}
+                key={index}
             >
                 {
-                    Object.keys(row).map((key) => {
+                    Object.keys(row).map((key, i) => {
                         return (
                             <TableCell
+                                key={`${index}_${i}`}
                                 style={{
                                     maxWidth: 150,
                                     overflow: "hidden",
@@ -110,7 +119,7 @@ class TabularView extends Component {
                             {this.renderTableHead()}
                         </TableHead>
                         <TableBody>
-                            {this.state.rows.map((row, index) => this.renderTableCells(row, index % 2 !== 0))}
+                            {this.state.rows.map((row, index) => this.renderTableCells(row, index))}
                         </TableBody>
                     </Table>
                 </TableContainer>
@@ -152,9 +161,10 @@ class TabularView extends Component {
                     >
                         <option aria-label={'None'} value={''}/>
                         {
-                            this.state.departments.map((department) => {
+                            this.state.departments.map((department, index) => {
                                 return (
                                     <option
+                                        key={index}
                                         value={department['department__name']}>
                                         {`${department['department__name']} (${department['count']})`}
                                     </option>
@@ -187,9 +197,10 @@ class TabularView extends Component {
                     >
                         <option aria-label={'None'} value={''}/>
                         {
-                            this.state.financialYears.map((fy) => {
+                            this.state.financialYears.map((fy, index) => {
                                 return (
                                     <option
+                                        key={index}
                                         value={fy['department__government__sphere__financial_year__slug']}>
                                         {`${fy['department__government__sphere__financial_year__slug']} (${fy['count']})`}
                                     </option>
@@ -222,11 +233,156 @@ class TabularView extends Component {
                     >
                         <option aria-label={'None'} value={''}/>
                         {
-                            this.state.frequencies.map((frequency) => {
+                            this.state.frequencies.map((frequency, index) => {
                                 return (
                                     <option
+                                        key={index}
                                         value={frequency['frequency']}>
                                         {`${frequency['frequency']} (${frequency['count']})`}
+                                    </option>
+                                )
+                            })
+                        }
+                    </Select>
+                </FormControl>
+            )
+        }
+    }
+
+    renderGovernmentFilter() {
+        if (this.state.governments === null) {
+            return <div></div>
+        } else {
+            return (
+                <FormControl variant={'outlined'} size={'small'}
+                             style={{minWidth: '160px', marginLeft: '20px', fontSize: '8px'}}>
+                    <InputLabel htmlFor={'frm-government'}>government</InputLabel>
+                    <Select
+                        native
+                        label={'government'}
+                        inputProps={{
+                            id: 'frm-government',
+                            name: 'department__government__name'
+                        }}
+                        value={this.state.selectedFilters['department__government__name']}
+                        onChange={(event) => this.handleFilterChange(event)}
+                    >
+                        <option aria-label={'None'} value={''}/>
+                        {
+                            this.state.governments.map((government, index) => {
+                                return (
+                                    <option
+                                        key={index}
+                                        value={government['department__government__name']}>
+                                        {`${government['department__government__name']} (${government['count']})`}
+                                    </option>
+                                )
+                            })
+                        }
+                    </Select>
+                </FormControl>
+            )
+        }
+    }
+
+    renderMtsfFilter() {
+        if (this.state.mtsfOutcomes === null) {
+            return <div></div>
+        } else {
+            return (
+                <FormControl variant={'outlined'} size={'small'}
+                             style={{minWidth: '160px', marginTop: '20px', fontSize: '8px'}}>
+                    <InputLabel htmlFor={'frm-mtsfOutcome'}>mtsf outcome</InputLabel>
+                    <Select
+                        native
+                        label={'mtsf outcome'}
+                        inputProps={{
+                            id: 'frm-mtsfOutcome',
+                            name: 'mtsf_outcome'
+                        }}
+                        value={this.state.selectedFilters['mtsf_outcome']}
+                        onChange={(event) => this.handleFilterChange(event)}
+                    >
+                        <option aria-label={'None'} value={''}/>
+                        {
+                            this.state.mtsfOutcomes.map((mtsfOutcome, index) => {
+                                return (
+                                    <option
+                                        key={index}
+                                        value={mtsfOutcome['mtsf_outcome']}>
+                                        {`${mtsfOutcome['mtsf_outcome']} (${mtsfOutcome['count']})`}
+                                    </option>
+                                )
+                            })
+                        }
+                    </Select>
+                </FormControl>
+            )
+        }
+    }
+
+    renderSectorFilter() {
+        if (this.state.sectors === null) {
+            return <div></div>
+        } else {
+            return (
+                <FormControl variant={'outlined'} size={'small'}
+                             style={{minWidth: '160px', marginLeft: '20px', fontSize: '8px'}}>
+                    <InputLabel htmlFor={'frm-sector'}>sector</InputLabel>
+                    <Select
+                        native
+                        label={'sector'}
+                        inputProps={{
+                            id: 'frm-sector',
+                            name: 'sector'
+                        }}
+                        value={this.state.selectedFilters['sector']}
+                        onChange={(event) => this.handleFilterChange(event)}
+                    >
+                        <option aria-label={'None'} value={''}/>
+                        {
+                            this.state.sectors.map((sector, index) => {
+                                return (
+                                    <option
+                                        key={index}
+                                        value={sector['sector']}>
+                                        {`${sector['sector']} (${sector['count']})`}
+                                    </option>
+                                )
+                            })
+                        }
+                    </Select>
+                </FormControl>
+            )
+        }
+    }
+
+    renderSphereFilter() {
+        if (this.state.spheres === null) {
+            return <div></div>
+        } else {
+            return (
+                <FormControl variant={'outlined'} size={'small'}
+                             style={{minWidth: '150px', marginLeft: '20px', marginTop: '20px', fontSize: '8px'}}>
+                    <InputLabel htmlFor={'frm-sphere'}>sphere</InputLabel>
+                    <Select
+                        native
+                        label={'sphere'}
+                        inputProps={{
+                            id: 'frm-sphere',
+                            name: 'department__government__sphere__name'
+                        }}
+                        value={this.state.selectedFilters['department__government__sphere__name']}
+                        onChange={(event) => this.handleFilterChange(event)}
+                    >
+                        <option aria-label={'None'} value={''}/>
+                        {
+                            this.state.spheres.map((sphere, index) => {
+                                return (
+                                    <option
+                                        key={index}
+                                        value={sphere['department__government__sphere__name']}>
+                                        {`${sphere['department__government__sphere__name']} (${sphere['count']})`}
                                     </option>
                                 )
                             })
@@ -243,6 +399,10 @@ class TabularView extends Component {
                 {this.renderDepartmentFilter()}
                 {this.renderFinancialYearFilter()}
                 {this.renderFrequencyFilter()}
+                {this.renderGovernmentFilter()}
+                {this.renderSectorFilter()}
+                {this.renderMtsfFilter()}
+                {this.renderSphereFilter()}
             </Grid>
         )
     }
