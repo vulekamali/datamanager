92126ef97ec928bb060309b8d9cd75fb338eb435
JD Bothma <jbothma@gmail.com>
2023-02-02 13:42:59 +0200

    Add basic full text search. Doesn't include related fields like department name yet

--- performance/models.py
+++ performance/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.search import SearchVectorField
+from django.contrib.postgres.indexes import GinIndex
 from django.db import models
 from django_q.tasks import Task
 
@@ -95,7 +97,7 @@ class Indicator(models.Model):
     annual_corrective_action = models.TextField(blank=True)
     annual_otp_comments = models.TextField(blank=True)
     annual_national_comments = models.TextField(blank=True)
-    annual_dpme_coordincator_comments = models.TextField(blank=True)
+    annual_dpme_coordinator_comments = models.TextField(blank=True)
     annual_treasury_comments = models.TextField(blank=True)
     annual_audited_output = models.TextField(blank=True)  # ValidatedAuditedSummary2
 
@@ -113,3 +115,8 @@ class Indicator(models.Model):
         EQPRSFileUpload, on_delete=models.CASCADE, related_name="indicator_values"
     )
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    content_search = SearchVectorField(null=True)
+
+    class Meta:
+        indexes = [GinIndex(fields=["content_search"])]
