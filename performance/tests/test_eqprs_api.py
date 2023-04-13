a5e0f548a8310cad0913f5c1ef18030f576ef3b0
emreyildirim2038@gmail.com <emreyildirim2038@gmail.com>
2023-04-13 13:33:47 +0300

    added automated tests

--- performance/tests/test_eqprs_api.py
+++ performance/tests/test_eqprs_api.py
@@ -2,6 +2,9 @@ from django.urls import reverse
 from rest_framework.test import APITestCase, APIRequestFactory
 from rest_framework import status
 from django.test import Client
+from openpyxl import load_workbook
+
+import io
 
 
 class indicator_API_Test(APITestCase):
@@ -9,7 +12,6 @@ class indicator_API_Test(APITestCase):
     fixtures = ["test_api.json"]
 
     def test_api(self):
-
         response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, 200)
         response_payload = self.client.get(self.list_url).json()
@@ -18,8 +20,8 @@ class indicator_API_Test(APITestCase):
         message1 = "1.2.1 Percentage of valid invoices paid within 30 days upon receipt by the department"
         message2 = "1.1.1 Unqualified audit opinion"
         if (
-            response_payload["results"]["items"][0]["indicator_name"] == message2
-            or response_payload["results"]["items"][1]["indicator_name"] == message1
+                response_payload["results"]["items"][0]["indicator_name"] == message2
+                or response_payload["results"]["items"][1]["indicator_name"] == message1
         ):
             found = True
 
@@ -43,3 +45,28 @@ class indicator_API_Test(APITestCase):
         filter_url = self.list_url + "?page=1&frequency=Annually"
         response_payload = self.client.get(filter_url).json()
         self.assertEqual(len(response_payload["results"]["items"]), 1)
+
+    def test_downloaded_file(self):
+        file_url = "/performance/performance-indicators.xlsx/"
+        response = self.client.get(file_url, stream=True)
+        file_content = response.getvalue()
+        file_obj = io.BytesIO(file_content)
+        wb = load_workbook(file_obj)
+        sh = wb['Sheet1']
+
+        self.assertEqual(sh['A1'].value, 'Financial year')
+        self.assertEqual(sh['A2'].value, '2015-16')
+        self.assertEqual(sh['H3'].value, 'Quarterly')
+        self.assertEqual(sh['A4'].value, None)
+
+    def test_downloaded_file_with_filter(self):
+        file_url = "/performance/performance-indicators.xlsx/?frequency=Annually"
+        response = self.client.get(file_url, stream=True)
+        file_content = response.getvalue()
+        file_obj = io.BytesIO(file_content)
+        wb = load_workbook(file_obj)
+        sh = wb['Sheet1']
+
+        self.assertEqual(sh['A1'].value, 'Financial year')
+        self.assertEqual(sh['H2'].value, 'Annually')
+        self.assertEqual(sh['A3'].value, None)
