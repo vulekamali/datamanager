76f29cc6f7f8a6f13338b109585f8cffa8facf31
emreyildirim2038@gmail.com <emreyildirim2038@gmail.com>
2023-06-05 22:11:19 +0300

    modifying iym columns

--- iym/admin.py
+++ iym/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from iym import models
 
+
 class IYMFileUploadAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = (
+        "import_report",
+        "user",
+        "process_completed"
+    )
+    fieldsets = (
+        (
+            "",
+            {
+                "fields": (
+                    "user",
+                    "financial_year",
+                    "latest_quarter",
+                    "file",
+                    "import_report",
+                    "process_completed",
+                )
+            },
+        ),
+    )
+
 
 admin.site.register(models.IYMFileUpload, IYMFileUploadAdmin)
--- iym/models.py
+++ iym/models.py
@@ -1,10 +1,28 @@
 from django.contrib.auth.models import User
 from django.db import models
+from budgetportal.models import FinancialYear
+import uuid
+
+QUARTERS = (
+    ("Q1", "Q1"),
+    ("Q2", "Q2"),
+    ("Q3", "Q3"),
+    ("Q4", "Q4"),
+)
+
+
+def eqprs_file_path(instance, filename):
+    return f"iym_uploads/{uuid.uuid4()}/{filename}"
+
 
 class IYMFileUpload(models.Model):
     user = models.ForeignKey(User, models.DO_NOTHING, blank=True)
-    task_id = models.TextField()
-    # Plain text listing which departments could not be matched and were not imported
+    financial_year = models.ForeignKey(
+        FinancialYear, on_delete=models.CASCADE
+    )
+    latest_quarter = models.CharField(max_length=2, choices=QUARTERS)
+    process_completed = models.BooleanField(default=False)
     import_report = models.TextField()
+    file = models.FileField(upload_to=eqprs_file_path)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
