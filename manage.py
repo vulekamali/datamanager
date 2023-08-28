8e2564741d521e92f55743b5b609758ad945e54e
paulmwatson <github.com@paulmwatson.com>
2023-08-28 20:40:40 +0200

    Black formatting

--- manage.py
+++ manage.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
+from psycogreen.gevent import patch_psycopg
 import os
 import sys
+
+from gevent import monkey
+
+monkey.patch_all()
+
+patch_psycopg()
+
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "budgetportal.settings")
