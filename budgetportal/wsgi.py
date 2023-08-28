24456d7c80eb470a423210d9c03ae8f7e6f9f585
JD Bothma <jbothma@gmail.com>
2023-08-28 07:55:56 +0200

    Try and fix postgres connection leak by implementing gevent properly

--- budgetportal/wsgi.py
+++ budgetportal/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from gevent import monkey
+monkey.patch_all()
+
+from psycogreen.gevent import patch_psycopg
+patch_psycopg()
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "budgetportal.settings")
 
 application = get_wsgi_application()
