a344c0209445644c94727b9f880bdd3e183f221b
paulmwatson <github.com@paulmwatson.com>
2023-09-07 12:27:56 +0200

    Clean up changes for psycogreen

--- budgetportal/wsgi.py
+++ budgetportal/wsgi.py
@@ -7,9 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+from psycogreen.gevent import patch_psycopg
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+from gevent import monkey
+
+monkey.patch_all()
+
+patch_psycopg()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "budgetportal.settings")
 
